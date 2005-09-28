1c77583e03f11a8d05ad7d28bfaeee58d7d147c8
Brian Mingus <mingus@grey.colorado.edu>
2005-09-28 22:25:58 +0000

    LGPL

--- src/taiqtso/iflowbox.h
+++ src/taiqtso/iflowbox.h
@@ -1,51 +1,18 @@
-// Copyright (C) 1995-2005 Regents of the University of Colorado,
+// Copyright, 1995-2005, Regents of the University of Colorado,
 // Carnegie Mellon University, Princeton University.
-// 
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-// 
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-/**********************************************************************
-** $Id: qt/qgroupbox.h   3.3.2   edited Jul 4 2003 $
-**
-** Definition of iFlowBox widget class
-**
-** Created : 950203
-**
-** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
-**
-** This file is part of the widgets module of the Qt GUI Toolkit.
-**
-** This file may be distributed under the terms of the Q Public License
-** as defined by Trolltech AS of Norway and appearing in the file
-** LICENSE.QPL included in the packaging of this file.
-**
-** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file.
-**
-** Licensees holding valid Qt Enterprise Edition or Qt Professional Edition
-** licenses may use this file in accordance with the Qt Commercial License
-** Agreement provided with the Software.
-**
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-**
-** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
-**   information about Qt Commercial License Agreements.
-** See http://www.trolltech.com/qpl/ for QPL licensing information.
-** See http://www.trolltech.com/gpl/ for GPL licensing information.
-**
-** Contact info@trolltech.com if any conditions of this licensing are
-** not clear to you.
-**
-**********************************************************************/
+//
+// This file is part of TA/CSS
+//
+//   This library is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU Lesser General Public
+//   License as published by the Free Software Foundation; either
+//   version 2.1 of the License, or (at your option) any later version.
+//   
+//   This library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   Lesser General Public License for more details.
+
 
 #ifndef IFLOWBOX_H
 #define IFLOWBOX_H
--- src/taiqtso/iwidgetlist.cpp
+++ src/taiqtso/iwidgetlist.cpp
@@ -1,51 +1,18 @@
-// Copyright (C) 1995-2005 Regents of the University of Colorado,
+// Copyright, 1995-2005, Regents of the University of Colorado,
 // Carnegie Mellon University, Princeton University.
-// 
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-// 
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-/* -*- C++ -*- */
-/*=============================================================================
-//									      //
-// This file is part of the TypeAccess/C-Super-Script software package.	      //
-//									      //
-// Copyright (C) 1995 Randall C. O'Reilly, Chadley K. Dawson, 		      //
-//		      James L. McClelland, and Carnegie Mellon University     //
-//     									      //
-// Permission to use, copy, modify, and distribute this software and its      //
-// documentation for any purpose is hereby granted without fee, provided that //
-// the above copyright notice and this permission notice appear in all copies //
-// of the software and related documentation.                                 //
-// 									      //
-// Note that the PDP++ software package, which contains this package, has a   //
-// more restrictive copyright, which applies only to the PDP++-specific       //
-// portions of the software, which are labeled as such.			      //
-//									      //
-// Note that the taString class, which is derived from the GNU String class,  //
-// is Copyright (C) 1988 Free Software Foundation, written by Doug Lea, and   //
-// is covered by the GNU General Public License, see ta_string.h.             //
-// The iv_graphic library and some iv_misc classes were derived from the      //
-// InterViews morpher example and other InterViews code, which is             //
-// Copyright (C) 1987, 1988, 1989, 1990, 1991 Stanford University             //
-// Copyright (C) 1991 Silicon Graphics, Inc.				      //
-//									      //
-// THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,         //
-// EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY 	      //
-// WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  	      //
-// 									      //
-// IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE FOR ANY SPECIAL,    //
-// INCIDENTAL, INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY KIND, OR ANY DAMAGES  //
-// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER OR NOT     //
-// ADVISED OF THE POSSIBILITY OF DAMAGE, AND ON ANY THEORY OF LIABILITY,      //
-// ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS        //
-// SOFTWARE. 								      //
-==============================================================================*/
+//
+// This file is part of TA/CSS
+//
+//   This library is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU Lesser General Public
+//   License as published by the Free Software Foundation; either
+//   version 2.1 of the License, or (at your option) any later version.
+//   
+//   This library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   Lesser General Public License for more details.
+
 
 #include "iwidgetlist.h"
 
