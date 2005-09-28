0b329031e003f7384a059631bd6604b082a9f7f3
Brian Mingus <mingus@grey.colorado.edu>
2005-09-28 21:40:06 +0000

    ta copyright update LGPL

--- src/ta/ta.h
+++ src/ta/ta.h
@@ -1,3 +1,15 @@
+// Copyright (C) 1995-2005 Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
 // The following ifdef block is the standard way of creating macros which make exporting 
 // from a DLL simpler. All files within this DLL are compiled with the TA_EXPORTS
 // symbol defined on the command line. this symbol should not be defined on any project
