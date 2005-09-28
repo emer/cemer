628541dcb797332e88c37e97b961a6f329f157fe
Brian Mingus <mingus@grey.colorado.edu>
2005-09-28 21:41:02 +0000

    taiqtso copyright update LGPL

--- src/taiqtso/iflowlayout.cpp
+++ src/taiqtso/iflowlayout.cpp
@@ -1,3 +1,15 @@
+// Copyright (C) 1995-2005 Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
 /****************************************************************************
 ** $Id: qt/examples/customlayout/flow.cpp   2.3.2   edited 2001-01-26 $
 **
