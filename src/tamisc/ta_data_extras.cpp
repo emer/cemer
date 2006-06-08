63079d42b7db7e9e0ccba12480cca93322c1b9d6
Brad Aisa <baisa@brad-aisa.com>
2006-06-08 01:39:43 +0000

    ImageReader and updates to Programs

---
+++ src/tamisc/ta_data_extras.cpp
@@ -0,0 +1,276 @@
+// Copyright, 1995-2005, Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+//
+// This file is part of TA/CSS
+//
+//   This library is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU Lesser General Public
+//   License as published by the Free Software Foundation; either
+//   version 2.1 of the License, or (at your option) any later version.
+//   
+//   This library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   Lesser General Public License for more details.
+
+
+
+#include "ta_data_extras.h"
+
+#include <QDir>
+
+extern "C" {
+// using the IV version of lib tiff!
+// #include <TIFF/tiffio.h>
+#include <jpeglib.h>
+}
+
+
+//////////////////////////
+//  DirectoryCatalog	//
+//////////////////////////
+
+void DirectoryCatalog::Initialize() {
+  m_dir = NULL;
+  directory = "./";
+}
+
+void DirectoryCatalog::Destroy() {
+  ReadClose();
+}
+
+QDir& DirectoryCatalog::dir() {
+  if (!m_dir)
+    m_dir = new QDir();
+  return *m_dir;
+}
+  
+const Variant DirectoryCatalog::GetData_impl(int chan) {
+  switch (chan) {
+  case 0: return dir()[rd_itr]; // blank if out of range
+  default: return _nilVariant;
+  }
+}
+
+int DirectoryCatalog::itemCount() const {
+  return m_dir->count();
+}
+  
+void DirectoryCatalog::ReadOpen_impl(bool& ok) {
+  inherited::ReadOpen_impl(ok);
+  if (!ok) return;
+  QDir& dir = this->dir(); // cache
+  dir.setPath(directory);
+  //TODO: need to refresh()?
+}
+
+void DirectoryCatalog::ReadClose_impl() {
+  if (m_dir) {
+    delete m_dir;
+    m_dir = NULL;
+  }
+  inherited::ReadClose_impl();
+}
+
+int DirectoryCatalog::sourceChannelCount() const {
+  return 1;
+}
+
+const String DirectoryCatalog::sourceChannelName(int chan) const {
+  static String chan_files("files");
+  switch (chan) {
+  case 0: return chan_files;
+  default: return _nilString;
+  }
+}
+
+
+//////////////////////////
+//  ImageReader		//
+//////////////////////////
+
+void ImageReader::Initialize() {
+  color_mode = CM_RGB;
+  img_format = IF_AUTO;
+  cur_img_format = img_format;
+  m_mat = NULL;
+}
+
+void ImageReader::Destroy() {
+  SetMat(NULL);
+}
+  
+void ImageReader::InitLinks() {
+  inherited::InitLinks();
+  taBase::Own(img_size, this);
+}
+
+void ImageReader::CutLinks() {
+  img_size.CutLinks();
+  inherited::CutLinks();
+}
+
+void ImageReader::Copy_(const ImageReader& cp) {
+  color_mode = cp.color_mode;
+  img_format = cp.img_format;
+  //dynamics get cleared
+  cur_img_format = IF_AUTO;
+  fname = _nilString;
+  SetMat(NULL);
+}
+
+taMatrix* ImageReader::GetDataMatrix_impl(int chan) {
+  if (!m_mat) return NULL;
+  
+  if (chan == chan_img)
+    return m_mat;
+    
+  switch (color_mode) {
+  case CM_GRAYSCALE:
+    switch (chan) {
+    case chan_bw: return m_mat->GetFrameSlice_(0);
+    default: return NULL;
+    }
+  case CM_RGB:
+    switch (chan) {
+    case chan_r: return m_mat->GetFrameSlice_(0);
+    case chan_g: return m_mat->GetFrameSlice_(1);
+    case chan_b: return m_mat->GetFrameSlice_(2);
+    default: return NULL;
+    }
+  }
+}
+
+void ImageReader::ReadClose_impl() {
+  if (m_mat) {
+    taBase::DelPointer((TAPtr*)&m_mat);
+  }
+  inherited::ReadClose_impl();
+}
+
+bool ImageReader::ReadImage_Jpeg() {
+  FILE* infile;
+  if ((infile = fopen(fname, "rb")) == NULL) {
+    taMisc::Warning("ImageReader::ReadImage_Jpeg: can't open file: ", fname);
+    return false;
+  }
+  
+  struct jpeg_decompress_struct cinfo;
+  struct jpeg_error_mgr jerr;
+
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_decompress(&cinfo);
+
+  jpeg_stdio_src(&cinfo, infile);
+  jpeg_read_header(&cinfo, TRUE);
+
+  // we should have dims now, so make matrices and set color space
+  img_size.x = cinfo.image_width; 
+  img_size.y = cinfo.image_height;
+  //note: jpeglib should interpolate or reduce output regardless of input file format
+  switch (color_mode) {
+  case CM_GRAYSCALE:
+    cinfo.out_color_space = JCS_GRAYSCALE; 
+    break;
+  case CM_RGB:
+    cinfo.out_color_space = JCS_RGB; 
+    break;
+  }
+  jpeg_start_decompress(&cinfo);
+  
+  // note: out size should = image size, since we aren't scaling
+  //   comps should be 1 for gray and 3 for color
+  int components = cinfo.output_components; 
+  byte_Matrix* bmat = new byte_Matrix(3, cinfo.output_width, cinfo.output_height, components);
+  SetMat(bmat);
+
+  int row_stride = cinfo.output_width;	/* JSAMPLEs per row in image_buffer */;
+  JSAMPLE* scanline = new JSAMPLE[row_stride * cinfo.rec_outbuf_height * components];
+
+  while (cinfo.output_scanline < cinfo.output_height) {
+    int lread = jpeg_read_scanlines(&cinfo, &scanline, cinfo.rec_outbuf_height);
+    int ctr = 0;
+    int srow; 
+    for (int y = 0; y < lread; y++) {
+      srow = cinfo.output_height - cinfo.output_scanline - y; 
+      for (int x = 0; x < (int)cinfo.output_width; ++x) {
+        for (int comp = 0; comp < components; ++comp) {
+          bmat->FastEl(x, srow, comp) = (byte)(scanline[ctr++]);
+        }
+      }
+    }
+  }
+
+  jpeg_finish_decompress(&cinfo);
+  jpeg_destroy_decompress(&cinfo);
+
+  fclose(infile);
+ 
+  delete [] scanline;
+  return true;
+}
+
+
+bool ImageReader::ReadItem_impl() {
+  switch (cur_img_format) {
+  case IF_JPEG: return ReadImage_Jpeg();
+  default: return false;
+  }
+}
+
+void ImageReader::ReadOpen_impl(bool& ok) {
+  inherited::ReadOpen_impl(ok);
+  if (!ok) return;
+  
+  // if in auto mode, try to guess based on file name
+  if (img_format == IF_AUTO) {
+    String tmp = upcase(fname);
+    if (tmp.contains(".JPG") || tmp.contains(".JPEG"))
+      cur_img_format = IF_JPEG;
+/*    else if (tmp.contains(".PNG"))
+      cur_img_format = IF_PNG;
+    else if (tmp.contains(".TIF") || tmp.contains(".TIFF"))
+      cur_img_format = IF_TIF; */
+  } else {
+    cur_img_format = img_format;
+  }
+}
+
+void ImageReader::SetMat(taMatrix* new_mat) {
+  taBase::SetPointer((TAPtr*)&m_mat, new_mat);
+}
+
+int ImageReader::sourceChannelCount() const {
+  int rval = 1; // base
+  switch (color_mode) {
+  case CM_GRAYSCALE: rval += 1; break;
+  case CM_RGB: rval += 3; break;
+  }
+  return rval;
+}
+
+const String ImageReader::sourceChannelName(int chan) const {
+//note: it is safe to not care what current channel count is, because access by name checks
+  static String nm_img("img");
+  static String nm_bw("bw");
+  static String nm_r("r");
+  static String nm_g("g");
+  static String nm_b("b");
+  if (chan == chan_img)
+    return nm_img;
+  
+  switch (color_mode) {
+  case CM_GRAYSCALE:
+    switch (chan) {
+    case chan_bw: return nm_bw;
+    default: return _nilString;
+    }
+  case CM_RGB:
+    switch (chan) {
+    case chan_r: return nm_r;
+    case chan_g: return nm_g;
+    case chan_b: return nm_b;
+    default: return _nilString;
+    }
+  }
+}
---
+++ src/tamisc/ta_data_extras.h
@@ -0,0 +1,125 @@
+// Copyright, 1995-2005, Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+//
+// This file is part of TA/CSS
+//
+//   This library is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU Lesser General Public
+//   License as published by the Free Software Foundation; either
+//   version 2.1 of the License, or (at your option) any later version.
+//   
+//   This library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   Lesser General Public License for more details.
+
+
+
+// ta_data_extras.h -- sundry useful DataBlock implementations
+
+#ifndef TA_DATA_EXTRAS_H
+#define TA_DATA_EXTRAS_H
+
+#include "ta_data.h"
+#include "tdgeometry.h"
+
+
+class TAMISC_API DirectoryCatalog: public DataBlock_Idx {// #INSTANCE a DirectoryCatalog provides a list of files in a directory
+INHERITED(DataBlock_Idx)
+public:
+  String		directory;  // path of directory where the files are
+  
+  TA_BASEFUNS(DirectoryCatalog)
+  
+protected:
+  QDir&			dir();
+  
+public: // DataBlock i/f
+  override DBOptions	dbOptions() const 
+    {return (DBOptions)(DB_INDEXABLE | DB_SEQUENCABLE | DB_SOURCE);}
+  override int		itemCount() const; 
+  
+public: // DataSource i/f
+  override int		sourceChannelCount() const;
+  override const String	sourceChannelName(int chan) const;
+  
+protected:
+  override const Variant GetData_impl(int chan);
+  override void		ReadOpen_impl(bool& ok);
+  override void		ReadClose_impl();
+
+private:
+  QDir*			m_dir; // autocreated, only access via dir()
+  void			Initialize();
+  void			Destroy();
+};
+
+
+
+class TAMISC_API ImageReader: public DataBlock {// #INSTANCE provides ability to read image files, sequential only
+INHERITED(DataBlock)
+public:
+  enum Channels {
+    chan_img		= 0, // the entire image matrix, [w+bord, h+bord, comps]
+    chan_bw		= 1, // GRAY: the b&w channel [w+bord, h+bord], 
+    chan_r		= 1, // RGB: the red channel [w+bord, h+bord]
+    chan_g		= 2, // RGB: the b&w channel [w+bord, h+bord]
+    chan_b		= 3  // RGB: the b&w channel [w+bord, h+bord]
+  };
+  
+  enum ColorMode { // the color mode to use for reading
+    CM_GRAYSCALE,	// read images in b&w only, 1 channel: bw (HxW byte array)
+    CM_RGB		// RGB mode, 3 channels: r, g, b (HxW byte arrays)
+  };
+  
+  enum ImageFormat { // the type of image encoding
+    IF_AUTO,		// try to determine automatically, ex. from filename, or file header
+    IF_JPEG		//
+  };
+  
+  ColorMode		color_mode; // color mode to use for reading
+  ImageFormat		img_format; // image format to use
+  String		fname; // #NO_SAVE file we are to read
+  
+  ImageFormat		cur_img_format; // #READ_ONLY #SHOW #NO_SAVE image format of current image
+  TwoDCoord 		img_size; // #READ_ONLY #SHOW #NO_SAVE size of current image
+  
+  
+  void	InitLinks();
+  void	CutLinks();
+  void	Copy_(const ImageReader& cp);
+  COPY_FUNS(ImageReader, DataBlock)
+  TA_BASEFUNS(ImageReader)
+  
+protected:
+  taMatrix*		m_mat; // we dynamically allocate each time, dims: [w h ch]
+  
+  bool 			ReadImage_Jpeg();
+  void			SetMat(taMatrix* new_mat);
+  
+public: // DataBlock i/f
+  override DBOptions	dbOptions() const 
+    {return (DBOptions)(DB_SEQUENCABLE | DB_SOURCE);}
+    
+  virtual bool		ReadNext() {return ReadItem_impl();}
+  
+public: // DataSource i/f
+  override int		sourceChannelCount() const; // depends on mode
+  override const String	sourceChannelName(int chan) const; // bw || r, g, b
+  
+  override bool		ReadItem_impl(); // actually does the read
+  override void		ReadOpen_impl(bool& ok);
+  override void		ReadClose_impl();
+protected:
+  override taMatrix*	GetDataMatrix_impl(int chan);
+
+private:
+  void			Initialize();
+  void			Destroy();
+};
+
+
+
+#endif
+
+
