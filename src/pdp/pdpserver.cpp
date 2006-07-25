63b72424c6ee90fc36117a3bcc8a93d4e69fc668
Brad Aisa <baisa@brad-aisa.com>
2006-07-25 11:08:12 +0000

    skeleton PdpServer tcp-based server added

---
+++ src/pdp/pdpserver.cpp
@@ -0,0 +1,162 @@
+// Copyright, 1995-2005, Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+//
+// This file is part of TA/PDP++
+//
+//   TA/PDP++ is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   TA/PDP++ is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+
+#include "pdpserver.h"
+
+#include <QHostAddress>
+#include <QTcpServer>
+#include <QTcpSocket>
+//TEMP
+#include <QByteArray>
+#include <QDataStream>
+// end TEMP
+
+//////////////////////////
+//  PdpServerAdapter	//
+//////////////////////////
+
+void PdpServerAdapter::server_newConnection() {
+  owner()->server_newConnection();
+}
+
+void PdpServerAdapter::socket_disconnected() {
+  owner()->socket_disconnected();
+}
+
+void PdpServerAdapter::socket_readyRead() {
+  owner()->socket_readyRead();
+}
+
+void PdpServerAdapter::socket_stateChanged(QAbstractSocket::SocketState socketState) {
+  owner()->socket_stateChanged(socketState);
+}
+
+
+
+//////////////////////////
+//  PdpServer		//
+//////////////////////////
+
+void PdpServer::Initialize() {
+  port = 5360;
+  open = false;
+  clients = 0;
+  server = NULL;
+  client = NULL;
+  SetAdapter(new PdpServerAdapter(this));
+  
+}
+
+void PdpServer::Destroy() {
+  CloseServer(false);
+}
+
+void PdpServer::CloseServer(bool notify) {
+  if (!open) return;
+  if (client) {
+    client->disconnectFromHost();
+    // probably called back immediately, so check again
+    if (client) {
+      client = NULL;
+      --clients;
+    }
+  }
+  if (server) {
+    delete server;
+    server = NULL;
+  }
+  open = false;
+  if (notify) DataChanged(DCR_ITEM_UPDATED);
+}
+
+bool PdpServer::OpenServer() {
+  if (open) return true;
+  server = new QTcpServer();
+  if (!server->listen(QHostAddress::Any, port)) {
+    taMisc::Error("Could not open the server: ",
+      server->errorString());
+    delete server;
+    server = NULL;
+    return false;
+  }
+  
+  QObject::connect(server, SIGNAL(newConnection()), adapter(), SLOT(server_newConnection()));
+
+  open = true;
+  DataChanged(DCR_ITEM_UPDATED);
+  return true;
+}
+
+void  PdpServer::server_newConnection() {
+  // setup data writing for hello or error block
+  QByteArray block;
+  QDataStream out(&block, QIODevice::WriteOnly);
+  out.setVersion(QDataStream::Qt_4_0);
+  
+  // get the latest connection, and always set it to self-destruct on close
+  QTcpSocket* ts = server->nextPendingConnection();
+  QObject::connect(ts, SIGNAL(disconnected()),
+    ts, SLOT(deleteLater()));
+  // we only allow 1 client (in this version of pdp), so refuse others
+  if (clients >= 1) {
+    out << "pdp4.0\n**ERROR** -- too many connections already, closing...\n";
+    ts->write(block);
+    ts->disconnectFromHost();
+    return;
+  }
+  client = ts;
+  ++clients;
+  QObject::connect(ts, SIGNAL(disconnected()),
+    adapter(), SLOT(socket_disconnected()));
+  QObject::connect(ts, SIGNAL(readyRead()),
+    adapter(), SLOT(socket_readyRead()));
+  QObject::connect(ts, SIGNAL(stateChanged(QAbstractSocket::SocketState)),
+    adapter(), SLOT(socket_stateChanged(QAbstractSocket::SocketState)));
+            
+    
+  out << "pdp4.0 \n"; // TODO: also output version (after the space)
+  ts->write(block);
+  DataChanged(DCR_ITEM_UPDATED);
+}
+
+void  PdpServer::socket_disconnected() {
+  //NOTE: only 1 allowed for now, so it must be our cached version
+  if (client == NULL) return; // already removed
+  client = NULL;
+  --clients;
+  DataChanged(DCR_ITEM_UPDATED);
+}
+
+void PdpServer::socket_readyRead() {
+  if (!client) return; 
+  qint64 ba = client->bytesAvailable(); // we use this to compare with readAll to check for error
+  QByteArray block = client->readAll(); //note: no way to report an error
+  if (block.count() != ba) {
+    taMisc::Error("read error on socket");
+  }
+
+//TEMP
+  // just echo for now
+  client->write(block);
+  
+// /TEMP
+}
+
+
+void PdpServer::socket_stateChanged(QAbstractSocket::SocketState socketState) {
+  //nothing yet
+}
+
---
+++ src/pdp/pdpserver.h
@@ -0,0 +1,92 @@
+// Copyright, 1995-2005, Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+//
+// This file is part of TA/PDP++
+//
+//   TA/PDP++ is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   TA/PDP++ is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+// pdpserver.h -- pdp server
+
+#ifndef PDPSERVER_H
+#define PDPSERVER_H
+
+#include "pdp_def.h"
+
+#include "ta_base.h"
+
+#include "pdpbase.h"
+#include "pdp_TA_type.h"
+
+#ifndef __MAKETA__
+# include <QAbstractSocket> // for state defines
+#endif
+
+// forwards
+class PdpServer;
+class PdpServerAdapter;
+
+class PDP_API PdpServerAdapter: public taBaseAdapter {
+  // ##IGNORE QObject for attaching events/signals for its taBase owner
+friend class PdpServer;
+  Q_OBJECT
+public:
+  inline PdpServer*	owner() {return (PdpServer*)taBaseAdapter::owner;}
+  PdpServerAdapter(PdpServer* owner_): taBaseAdapter((taOBase*)owner_) {}
+  
+#ifndef __MAKETA__ // maketa chokes on the net class types etc.
+public slots:
+  void 			server_newConnection();
+  
+  // socket functions -- currently on this obj, but if we allowed multi, have to create
+  // separate QObject handler for each one, because Qt doesn't put the sender in the calls
+  void 			socket_readyRead();
+  void 			socket_disconnected(); //note: we only allow one for now, so monitor it here
+  void 			socket_stateChanged(QAbstractSocket::SocketState socketState);
+#endif
+};
+
+class PDP_API PdpServer: public taOBase { 
+  // #INSTANCE #TOKENS Pdp Server, for tcp-based remote services 
+INHERITED(taOBase)
+public:
+  unsigned short	port; // #DEF_5360 port number to use -- each instance must have unique port
+  bool			open; // #NO_SAVE #SHOW #READ_ONLY set when server is open and accepting connections
+  int			clients; // #SHOW #NO_SAVE #READ_ONLY how many clients are connected
+  
+  inline PdpServerAdapter* adapter() {return (PdpServerAdapter*)taOBase::adapter;} // #IGNORE
+
+  bool			OpenServer(); // #BUTTON #GHOST_ON_open open the server and accept connections
+  void			CloseServer(bool notify = true); // #BUTTON #GHOST_OFF_open #ARGC_0 stop the server and close open connections
+  
+  
+  TA_BASEFUNS(PdpServer);
+
+#ifndef __MAKETA__ // maketa chokes on the net class types etc.
+public: // slot forwardees
+  void 			server_newConnection(); //
+  
+  // socket functions -- currently on this obj, but if we allowed multi, have to create
+  // separate QObject handler for each one, because Qt doesn't put the sender in the calls
+  void 			socket_readyRead();
+  void 			socket_disconnected(); //note: we only allow one for now, so monitor it here
+  void 			socket_stateChanged(QAbstractSocket::SocketState socketState);
+#endif
+
+protected:
+  QTcpServer*		server; // #IGNORE
+  
+  QTcpSocket*		client; // #IGNORE the one and only socket we allow to connect
+private:
+  void	Initialize();
+  void 	Destroy();
+};
+
+#endif
