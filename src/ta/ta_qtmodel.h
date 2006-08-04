4a77aafa0b2aabcc33ac7ff5d428d39f8d224624
Brad Aisa <baisa@brad-aisa.com>
2006-08-04 22:09:16 +0000

    stub files for qtmodel

---
+++ src/ta/ta_qtmodel.h
@@ -0,0 +1,29 @@
+//
+// This file is part of TA/CSS
+//
+//   This library is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU Lesser General Public
+//   License as published by the Free Software Foundation; either
+//   version 2.1 of the License, or (at your option) any later version.
+//   
+//   This library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   Lesser General Public License for more details.
+
+
+// ta_qtmodel.h: Qt models for type and token browsing
+
+#ifndef TA_QTMODEL_H
+#define TA_QTMODEL_H
+
+#include "ta_group.h"
+#include "ta_variant.h"
+
+#ifndef __MAKETA__
+# include <QAbstractItemModel>
+#endif
+
+
+#endif
+
