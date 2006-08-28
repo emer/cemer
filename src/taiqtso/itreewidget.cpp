c4da92031198a6bcaf6b339417a7785c0cf1f7a7
Brad Aisa <baisa@brad-aisa.com>
2006-08-28 00:45:53 +0000

    qt4 treeview, basic clip seems to work, no drag-drop ye

---
+++ src/taiqtso/itreewidget.cpp
@@ -0,0 +1,179 @@
+// Copyright, 1995-2005, Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+//
+// This file is part of TA/CSS
+//
+//   This library is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU Lesser General Public
+//   License as published by the Free Software Foundation; either
+//   version 2.1 of the License, or (at your option) any later version.
+//   
+//   This library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   Lesser General Public License for more details.
+
+#include "itreewidget.h"
+
+iTreeWidget::iTreeWidget(QWidget* parent)
+:inherited(parent)
+{
+  connect(this, SIGNAL(itemExpanded(QTreeWidgetItem*)),
+    this,  SLOT(this_itemExpanded(QTreeWidgetItem*)) );
+}
+
+bool iTreeWidget::allColumnsShowFocus() const {
+//NOTE: assuming the following test will return this
+  return (selectionBehavior() & QAbstractItemView::SelectRows);
+}
+
+
+//NOTE: this routine copied and modified from Qt3 source
+void iTreeWidget::contextMenuEvent(QContextMenuEvent* e)
+{
+  if (!receivers( SIGNAL(contextMenuRequested(QTreeWidgetItem*,const QPoint&,int)) ) ) {
+      e->ignore();
+      return;
+  }
+  if (e->reason() == QContextMenuEvent::Keyboard ) {
+    QTreeWidgetItem* item = currentItem();
+    if (item) {
+      QRect r = visualItemRect(item);
+      QPoint p = r.topLeft();
+      if (allColumnsShowFocus() )
+        p += QPoint( width() / 2, ( r.height() / 2 ) );
+      else
+        p += QPoint( columnWidth( 0 ) / 2, ( r.height() / 2 ) );
+      p.rx() = QMAX( 0, p.x() );
+      p.rx() = QMIN( visibleRegion().boundingRect().width(), p.x() ); //TEST
+      emit contextMenuRequested(item, viewport()->mapToGlobal( p ), -1 );
+    }
+  } else {
+//    QPoint vp = contentsToViewport( e->pos() );
+    QPoint vp = e->pos() ; //TODO: prob needs transform
+    QTreeWidgetItem* i = itemAt( vp );
+//    int c = i ? d->h->mapToLogical( d->h->cellAt( vp.x() ) ) : -1;
+    int c = 0; // TODO
+    emit contextMenuRequested( i, viewport()->mapToGlobal( vp ), c );
+  }
+}
+
+void iTreeWidget::doItemExpanded(QTreeWidgetItem* item_, bool expanded) {
+  // safe cast, null if not of type
+  iTreeWidgetItem* item = dynamic_cast<iTreeWidgetItem*>(item_);
+  if (item)
+    item->itemExpanded(expanded);
+}
+
+void iTreeWidget::dropEvent(QDropEvent* e) {
+  drop_pos = e->pos();
+  inherited::dropEvent(e);
+}
+
+bool iTreeWidget::dropMimeData(QTreeWidgetItem* parent, int index, 
+  const QMimeData* data, Qt::DropAction action) 
+{
+  iTreeWidgetItem* item = NULL;
+  if (parent) 
+    item = dynamic_cast<iTreeWidgetItem*>(parent->child(index));
+  else // presumably then it is root item on us...
+    item = dynamic_cast<iTreeWidgetItem*>(topLevelItem(index));
+  if (!item) return false;
+  item->dropped(data, drop_pos);
+  return false;
+}
+
+QMimeData* iTreeWidget::mimeData(const QList<QTreeWidgetItem *> items) const {
+  if (items.count() == 0) return NULL; // presumably shouldn't happen
+  else if (items.count() == 1) {
+      iTreeWidgetItem* item = dynamic_cast<iTreeWidgetItem*>(items.at(0));
+      if (item) 
+        return mimeDataSingle(item);
+  } else {
+    QList<iTreeWidgetItem*> items2;
+    for (int i = 0; i < items.count(); ++i) {
+      iTreeWidgetItem* item = dynamic_cast<iTreeWidgetItem*>(items.at(i));
+      if (item) items2.append(item);
+    }
+    if (items2.count() > 0)
+      return mimeDataMulti(items2);
+  }
+  return NULL;
+}
+
+QMimeData* iTreeWidget::mimeDataSingle(iTreeWidgetItem* item) const {
+  return item->mimeData();
+}
+
+void iTreeWidget::this_itemCollapsed(QTreeWidgetItem* item) {
+  doItemExpanded(item, false);
+}
+
+void iTreeWidget::this_itemExpanded(QTreeWidgetItem* item) {
+  doItemExpanded(item, true);
+}
+
+
+//////////////////////////
+//  iTreeWidgetItem	//
+//////////////////////////
+
+iTreeWidgetItem::iTreeWidgetItem(iTreeWidget* parent)
+:inherited(parent)
+{
+  init();
+}
+
+iTreeWidgetItem::iTreeWidgetItem(iTreeWidget* parent, iTreeWidgetItem* preceding)
+:inherited(parent, preceding)
+{
+  init();
+}
+
+iTreeWidgetItem::iTreeWidgetItem(iTreeWidgetItem* parent)
+:inherited(parent)
+{
+  init();
+}
+
+iTreeWidgetItem::iTreeWidgetItem(iTreeWidgetItem* parent, iTreeWidgetItem* preceding)
+:inherited(parent, preceding)
+{
+  init();
+}
+
+void iTreeWidgetItem::init() {
+  lazy_children = false;
+  children_created = false;
+}
+
+void iTreeWidgetItem::enableLazyChildren() {
+  if (lazy_children || (childCount() > 0) ) return;
+  lazy_children = true; // do first, in case any signals happen during following...
+  new QTreeWidgetItem(this); // just a placeholder, gets deleted
+}
+
+void iTreeWidgetItem::CreateChildren() {
+  if (lazy_children) {
+    // should only be one child -- prob something went wrong if not
+    int cc = childCount();
+    if (cc > 1) {
+      qWarning("iTreeWidgetItem::createLazyChildren: 1 dummy item expected; %i items encountered.\n", cc);
+    } else if (cc == 1) { // expected case
+      delete takeChild(0); // the dummy
+    } // 0 case shouldn't happen
+    lazy_children = false;
+  }
+  CreateChildren_impl();
+  children_created = true;
+}
+
+bool iTreeWidgetItem::isExpanded() const {
+  QTreeWidget* tw = treeWidget();
+  return (tw && tw->isItemExpanded(this));
+}
+
+void iTreeWidgetItem::itemExpanded(bool expanded) {
+  if (!expanded || !lazy_children) return;
+  CreateChildren();
+}
---
+++ src/taiqtso/itreewidget.h
@@ -0,0 +1,95 @@
+// Copyright, 1995-2005, Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+//
+// This file is part of TA/CSS
+//
+//   This library is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU Lesser General Public
+//   License as published by the Free Software Foundation; either
+//   version 2.1 of the License, or (at your option) any later version.
+//   
+//   This library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   Lesser General Public License for more details.
+
+
+#ifndef ITREEWIDGET_H
+#define ITREEWIDGET_H
+
+#include "taiqtso_def.h"
+
+#include <QEvent>
+#include <QPoint>
+#include <QTreeWidget>
+
+class iTreeWidgetItem;
+
+class TAIQTSO_API iTreeWidget: public QTreeWidget { 
+INHERITED(QTreeWidget)
+  Q_OBJECT
+public:
+  bool			allColumnsShowFocus() const; // qt3 compat
+  
+  iTreeWidget(QWidget* parent = 0);
+  
+signals:
+  void 			contextMenuRequested(QTreeWidgetItem* item, const QPoint& pos, int col);
+  
+protected:
+  QPoint		drop_pos; // we capture this from the drop event
+  
+  void			dropEvent(QDropEvent* e); // override
+  bool 			dropMimeData(QTreeWidgetItem* parent, int index, 
+    const QMimeData* data, Qt::DropAction action); // override -- we always delegate to the item, and always return false (we handle item manipulation manually)
+  void 			contextMenuEvent(QContextMenuEvent* e); // override
+  QMimeData* 		mimeData(const QList<QTreeWidgetItem *> items) const; // override -- we supply mimeData in our own internal formats, not those of tree items
+  void			doItemExpanded(QTreeWidgetItem* item, bool expanded);
+  
+protected: // new overridables
+  virtual QMimeData* 	mimeDataSingle(iTreeWidgetItem* item) const; // default delegates to item
+  virtual QMimeData* 	mimeDataMulti(const QList<iTreeWidgetItem*> items) const
+    {return NULL;} // note: there will normally be 2 or more items in the list
+  
+protected slots:
+  void			this_itemExpanded(QTreeWidgetItem* item);
+  void			this_itemCollapsed(QTreeWidgetItem* item);
+};
+
+
+class TAIQTSO_API iTreeWidgetItem: public QTreeWidgetItem { 
+INHERITED(QTreeWidgetItem)
+friend class iTreeWidget;
+public:
+  
+  bool			isExpanded() const;
+  inline bool		lazyChildren() const {return lazy_children;}
+    // true while lc enabled, but not yet expanded
+  void			enableLazyChildren(); // call on create or anytime when empty
+  
+  virtual bool		acceptDrop(const QMimeData* mime) const {return false;}
+    // returns whether we can accept the given data in a drop upon us
+  
+  virtual void		CreateChildren(); // creates the children, called automatically on expand if lazy_children; normally override _impl
+  
+
+  iTreeWidgetItem(iTreeWidget* parent);
+  iTreeWidgetItem(iTreeWidget* parent, iTreeWidgetItem* preceding);
+  iTreeWidgetItem(iTreeWidgetItem* parent);
+  iTreeWidgetItem(iTreeWidgetItem* parent, iTreeWidgetItem* preceding);
+  
+protected:
+  uint			lazy_children : 1;
+  uint			children_created : 1;
+  
+  virtual void		dropped(const QMimeData* mime, const QPoint& pos) {}
+    // what to do when data dropped, usually we put up a drop context menu
+  virtual void		itemExpanded(bool expanded); // called when exanded or closed
+  virtual QMimeData*	mimeData() const {return NULL;} // called for single item src request
+  virtual void		CreateChildren_impl() {} // override this to create the true children
+private:
+  void			init();
+};
+
+
+#endif
