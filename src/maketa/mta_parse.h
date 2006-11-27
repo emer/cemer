18117fc5ae56b3cffbed37175b9d38c6f9a84375
Brad Aisa <baisa@brad-aisa.com>
2006-11-27 16:51:44 +0000

    suggested changes to docs

--- src/maketa/mta_parse.h
+++ src/maketa/mta_parse.h
@@ -1,4 +1,98 @@
-typedef struct {
+/* A Bison parser, made by GNU Bison 2.1.  */
+
+/* Skeleton parser for Yacc-like parsing with Bison,
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, when this file is copied by Bison into a
+   Bison output file, you may use that output file without restriction.
+   This special exception was added by the Free Software Foundation
+   in version 1.24 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     CLASS = 258,
+     STRUCT = 259,
+     UNION = 260,
+     ENUM = 261,
+     FUNTYPE = 262,
+     STATIC = 263,
+     TEMPLATE = 264,
+     CONST = 265,
+     TYPEDEF = 266,
+     TA_TYPEDEF = 267,
+     TYPE = 268,
+     NUMBER = 269,
+     FUNCTION = 270,
+     ARRAY = 271,
+     NAME = 272,
+     COMMENT = 273,
+     FUNCALL = 274,
+     SCOPER = 275,
+     EQUALS = 276,
+     PUBLIC = 277,
+     PRIVATE = 278,
+     PROTECTED = 279,
+     OPERATOR = 280,
+     FRIEND = 281,
+     THISNAME = 282,
+     REGFUN = 283,
+     VIRTUAL = 284
+   };
+#endif
+/* Tokens.  */
+#define CLASS 258
+#define STRUCT 259
+#define UNION 260
+#define ENUM 261
+#define FUNTYPE 262
+#define STATIC 263
+#define TEMPLATE 264
+#define CONST 265
+#define TYPEDEF 266
+#define TA_TYPEDEF 267
+#define TYPE 268
+#define NUMBER 269
+#define FUNCTION 270
+#define ARRAY 271
+#define NAME 272
+#define COMMENT 273
+#define FUNCALL 274
+#define SCOPER 275
+#define EQUALS 276
+#define PUBLIC 277
+#define PRIVATE 278
+#define PROTECTED 279
+#define OPERATOR 280
+#define FRIEND 281
+#define THISNAME 282
+#define REGFUN 283
+#define VIRTUAL 284
+
+
+
+
+#if ! defined (YYSTYPE) && ! defined (YYSTYPE_IS_DECLARED)
+#line 44 "mta_parse.y"
+typedef struct YYSTYPE {
   TypeDef* 	typ;
   EnumDef* 	enm;
   MemberDef* 	memb;
@@ -6,33 +100,14 @@ typedef struct {
   char*    	chr;
   int	   	rval;
 } YYSTYPE;
-#define	CLASS	257
-#define	STRUCT	258
-#define	UNION	259
-#define	ENUM	260
-#define	FUNTYPE	261
-#define	STATIC	262
-#define	TEMPLATE	263
-#define	CONST	264
-#define	TYPEDEF	265
-#define	TA_TYPEDEF	266
-#define	TYPE	267
-#define	NUMBER	268
-#define	FUNCTION	269
-#define	ARRAY	270
-#define	NAME	271
-#define	COMMENT	272
-#define	FUNCALL	273
-#define	SCOPER	274
-#define	EQUALS	275
-#define	PUBLIC	276
-#define	PRIVATE	277
-#define	PROTECTED	278
-#define	OPERATOR	279
-#define	FRIEND	280
-#define	THISNAME	281
-#define	REGFUN	282
-#define	VIRTUAL	283
-
+/* Line 1447 of yacc.c.  */
+#line 105 "y.tab.h"
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
 
 extern YYSTYPE yylval;
+
+
+
