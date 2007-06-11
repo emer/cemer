9291cd6d439389d853d4ca31f4c4101dd997d4ad
Brad Aisa <baisa@brad-aisa.com>
2007-06-11 18:46:02 +0000

    Increased GenDoc filtering

--- src/maketa/mta_gendoc.cpp
+++ src/maketa/mta_gendoc.cpp
@@ -21,6 +21,11 @@
 #include "ta/ta_platform.h"
 #include "ta/ta_type.h"
 
+// TODO: Put everything/methods/members/etc... into wiki categories based on CAT_
+// The logical ordering is: enum, subtypes, members, methods
+// For member options, it goes guys with nothing, then expert, then hidden
+//  * Hidden also includes readonly
+
 //////////////////////////////////
 // 	     GenDoc		//
 //////////////////////////////////
@@ -45,125 +50,141 @@ void MTA::GenDoc(TypeSpace* ths, fstream& strm) {
     TypeDef* td = ts->FastEl(i);
 
     // We only want "actual" types, not pointers or references to types, etc...
-    if (!td->ptr && !td->ref && !td->formal && !td->pre_parsed) {
-      strm << "  <TypeDef name=\"" << td->name;
-      strm << (td->is_enum() ? "\" type=\"enum" : "");
-      strm << (td->is_class() ? "\" type=\"class" : "");
-      strm << "\">\n";
+    if (td->ptr || td->ref || td->formal || td->pre_parsed) 
+      continue;
 
-      // Options of this TypeDef
-      //strm << "   <options>" << td->opts.AsString() << "</options>\n";
+    // exclude template instances that have a single further subclass
+    if (td->InheritsFormal(TA_templ_inst) && (td->children.size == 1))
+     continue;
+    
+    strm << "  <TypeDef name=\"" << td->name;
+    strm << (td->is_enum() ? "\" type=\"enum" : "");
+    strm << (td->is_class() ? "\" type=\"class" : "");
+    strm << "\">\n";
+
+    // Options of this TypeDef
+    //strm << "   <option>" << td->opts.AsString() << "</option>\n";
       
-      // Parents of this TypeDef
-      TypeSpace* tsp = &td->parents;
-      for (int j=0; j < tsp->size; j++) {
-	TypeDef* tdp = tsp->FastEl(j);
-	strm << "   <parent>" << tdp->name << "</parent>\n";
-      }
+    // Parents of this TypeDef
+    TypeSpace* tsp = &td->parents;
+    for (int j=0; j < tsp->size; j++) {
+      TypeDef* tdp = tsp->FastEl(j);
+      strm << "   <parent>" << tdp->name << "</parent>\n";
+    }
 
-      // Children of this TypeDef
-      TypeSpace* tsc = &td->children;
-      for (int j=0; j < tsc->size; j++) {
-	TypeDef* tdc = tsc->FastEl(j);
-	strm << "   <children>" << tdc->name << "</children>\n";
-      }
+    // Children of this TypeDef
+    TypeSpace* tsc = &td->children;
+    for (int j=0; j < tsc->size; j++) {
+      TypeDef* tdc = tsc->FastEl(j);
+      strm << "   <children>" << tdc->name << "</children>\n";
+    }
+
+    //////////////////////
+    //    MemberSpace   //
+    //////////////////////
+
+    MemberSpace* mems = &td->members;
+
+    if (mems->size) {
+      strm << "   <MemberSpace name=\"" << mems->name << "\">\n";
 
       //////////////////////
-      //    MemberSpace   //
+      //    MemberDef     //
       //////////////////////
 
-      MemberSpace* mems = &td->members;
+      for (int j=0; j < mems->size; j++) {
+	MemberDef* md = mems->FastEl(j);
 
-      if (mems->size) {
-	strm << "   <MemberSpace name=\"" << mems->name << "\">\n";
+	// Shortened src/ta.xml by !5k lines
+	if (MemberSpace_Filter_Member(mems, md))
+	  continue;
 
-        //////////////////////
-        //    MemberDef     //
-        //////////////////////
+	strm << "     <MemberDef name=\"" << md->name << ">\n";
 
-	for (int j=0; j < mems->size; j++) {
-	  MemberDef* md = mems->FastEl(j);
+	strm << (md->desc.length() > 2 ? "      <desc>" + md->desc + "</desc>\n" : "");
+	if (md->opts.size) {
+	  for (int k=0; k < md->opts.size; k++ ) {
+	    strm << "      <options>" << md->opts.FastEl(k) << "</options>\n";
+	  }
+	}
+	strm << "     </MemberDef>\n";
+      }
+      strm << "   </MemberSpace>\n";
+    }
 
-	  strm << "     <MemberDef name=\"" << md->name << "\" ";
-	  // TODO: These are all false
-	  strm << "offset=\"" << md->base_off <<"\" ";
-	  strm << "static=\"" << md->is_static <<"\" ";
-	  strm << "fun_ptr=\"" << md->fun_ptr <<"\" ";
+    //////////////////////
+    //    MethodSpace   //
+    //////////////////////
+
+    MethodSpace* mets = &td->methods;
+    if (mets->size) {
+      strm << "   <MethodSpace\">\n";
+
+      //////////////////////
+      //    MethodDef     //
+      //////////////////////
+
+      for (int j=0; j < mets->size; j++) {
+	MethodDef* metd = mets->FastEl(j);
+
+	// Shortened src/ta.xml by ~20k lines
+	if (MethodSpace_Filter_Method(mets, metd))
+	  continue;
+
+	int metd_desc_len = metd->desc.length();
+	int metd_opts_size = metd->opts.size;
+	int metd_args_size = metd->arg_types.size;
+	bool metd_cont = 1;
+	strm << "     <MethodDef name=\"" << metd->name << "\"";
+
+	// Make this a self-closing tag if it has no xml children
+	if (metd_desc_len < 2 && !metd_opts_size) {
+	  strm << " />\n";
+	  metd_cont = 0;
+	}
+	  
+	if (metd_cont) {
 	  strm << ">\n";
 
-	  strm << (md->desc.length() > 2 ? "      <desc>" + md->desc + "</desc>\n" : "");
-	  if (md->opts.size) {
-	    for (int k=0; k < md->opts.size; k++ ) {
-	      strm << "      <options>" << md->opts.FastEl(k) << "</options>\n";
+	  // Get the prototype
+	    strm << "      <prototype>" + metd->prototype() + "</prototype>\n";
+	  // Get the description
+	  if (metd_desc_len > 2) {
+	    strm << "      <desc>" + metd->desc + "</desc>\n";
+	  }
+	  // Get the options
+	  if (metd_opts_size) {
+	    for (int k=0; k < metd->opts.size; k++ ) {
+	      strm << "      <option>" << metd->opts.FastEl(k) << "</option>\n";
 	    }
 	  }
-	  strm << "     </MemberDef>\n";
+	  strm << "     </MethodDef>\n";
 	}
-	strm << "   </MemberSpace>\n";
       }
-
-      //////////////////////
-      //    MethodSpace   //
-      //////////////////////
-
-      MethodSpace* mets = &td->methods;
-      if (mets->size) {
-	strm << "   <MethodSpace\">\n";
-
-        //////////////////////
-        //    MethodDef     //
-        //////////////////////
-
-	for (int j=0; j < mets->size; j++) {
-	  MethodDef* metd = mets->FastEl(j);
-	  int metd_desc_len = metd->desc.length();
-	  int metd_opts_size = metd->opts.size;
-	  bool metd_cont = 1;
-	  strm << "     <MethodDef name=\"" << metd->name << "\"";
-
-	  // Make this a self-closing tag if it has no xml children
-	  if (metd_desc_len < 2 && !metd_opts_size) {
-	    strm << " />\n";
-	    metd_cont = 0;
-	  }
-	  
-	  if (metd_cont) {
-	    strm << ">\n";
-	    if (metd->desc.length() > 2) {
-	      strm << "      <desc>" + metd->desc + "</desc>\n";
-	    }
-	    if (metd->opts.size) {
-	      for (int k=0; k < metd->opts.size; k++ ) {
-		strm << "      <options>" << metd->opts.FastEl(k) << "</options>\n";
-	      }
-	    }
-	    strm << "     </MethodDef>\n";
-	  }
-	}
-	strm << "   </MethodSpace>\n";
-      }
-
-      //////////////////////
-      //    EnumSpace     //
-      //////////////////////
-
-      // Note: These are self-closing tags
-      EnumSpace* es = &td->enum_vals;
-      if(es->size) {
-	strm << "   <EnumSpace>\n";
-
-        //////////////////////
-        //    EnumDef       //
-        //////////////////////
-
-	for (int j=0; j < es->size; j++) {
-	  EnumDef* ed = es->FastEl(j);
-	  strm << "     <EnumDef name=\"" << ed->name << "\" " << "value=\"" << ed->enum_no << "\"/>\n";
-	}
-	strm << "   </EnumSpace>\n";
-      }
-      strm << "  </TypeDef>\n";
+      strm << "   </MethodSpace>\n";
     }
+
+    //////////////////////
+    //    EnumSpace     //
+    //////////////////////
+
+    // Note: These are self-closing tags
+    EnumSpace* es = &td->enum_vals;
+    if(es->size) {
+      strm << "   <EnumSpace>\n";
+
+      //////////////////////
+      //    EnumDef       //
+      //////////////////////
+
+      for (int j=0; j < es->size; j++) {
+	EnumDef* ed = es->FastEl(j);
+	strm << "     <EnumDef name=\"" << ed->name << "\" " << "value=\"" << ed->enum_no << "\"/>\n";
+      }
+      strm << "   </EnumSpace>\n";
+    }
+    strm << "  </TypeDef>\n";
   }
   strm << " </TypeSpace>\n";
+  strm << "</xml>\n";
 }
