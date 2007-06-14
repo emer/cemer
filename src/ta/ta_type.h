47ceda9fe979ebc9c9fd2fc1f4a7057a4d574773
Brad Aisa <baisa@brad-aisa.com>
2007-06-14 16:07:59 +0000

    SVN_REV in header also

--- src/ta/ta_type.h
+++ src/ta/ta_type.h
@@ -606,8 +606,10 @@ public:
   static String		version; // #READ_ONLY #NO_SAVE #SHOW version number of ta/css
   static taVersion 	version_bin; 
    //  #READ_ONLY #NO_SAVE #EXPERT version number of ta/css
+#ifdef SVN_REV
   static String		svn_rev; 
    // #READ_ONLY #NO_SAVE #EXPERT svn revision number (only valid when configure has been rerun)
+#endif
   static const BuildType build_type; // #READ_ONLY #NO_SAVE #SHOW build type, mostly for determining plugin subfolders to search
   static const String	build_str; // #READ_ONLY #NO_SAVE #EXPERT an extension string based on build type, mostly for plugin subfolders (none for release gui no-dmem) 
   
