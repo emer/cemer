016a4f9f5e56dc70b233c5a8027170ab6bfa9c49
Brad Aisa <baisa@brad-aisa.com>
2007-11-19 19:09:44 +0000

    license change in ta_string

--- src/temt/ta/ta_string.cpp
+++ src/temt/ta/ta_string.cpp
@@ -14,21 +14,15 @@
 //   Lesser General Public License for more details.
 
 
-/*
-Copyright (C) 1988 Free Software Foundation
+/* HISTORICAL NOTE
+
+Earlier versions of this file were based on the GNU String class
     written by Doug Lea (dl@rocky.oswego.edu)
 
-This file is part of the GNU C++ Library.  This library is free
-software; you can redistribute it and/or modify it under the terms of
-the GNU Library General Public License as published by the Free
-Software Foundation; either version 2 of the License, or (at your
-option) any later version.  This library is distributed in the hope
-that it will be useful, but WITHOUT ANY WARRANTY; without even the
-implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the GNU Library General Public License for more details.
-You should have received a copy of the GNU Library General Public
-License along with this library; if not, write to the Free Software
-Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+The class was later completely rewritten to use counting semantics, and
+it is the opinion of the authors that this new class represents an original
+software work for purposes of copyright.
+
 */
 
 /*
