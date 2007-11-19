54074bf68d5b57019627d43ab19750cdcc9e0ae9
Brad Aisa <baisa@brad-aisa.com>
2007-11-19 20:09:11 +0000

    new test

--- bench/core_bench/cc_ptest_core_mac.sh
+++ bench/core_bench/cc_ptest_core_mac.sh
@@ -1,10 +1,11 @@
 #!/bin/csh -f
 
 # cocor command
-set CC = "g++ -O2"
-set CCd = "g++ -g -O2"
+set CC = "g++"
+set CCd = "g++"
 echo $CC
-$CC -o ptest_core ptest_core.cc -lm -Wl,-framework,QtCore -L/Library/Frameworks
+$CC -O2 -S ptest_core2.cc  -L/Library/Frameworks
+$CC -O2 -o ptest_core2 ptest_core2.cc -lm -Wl,-framework,QtCore -L/Library/Frameworks
 echo $CCd
-$CCd -o ptest_cored ptest_core.cc -lm -Wl,-framework,QtCore -L/Library/Frameworks
+$CCd -g -o ptest_core2d ptest_core2.cc -lm -Wl,-framework,QtCore -L/Library/Frameworks
 
