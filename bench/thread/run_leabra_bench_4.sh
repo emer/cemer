5e82c26ddc50a7569ff730a117bedc50ae468073
Brad Aisa <baisa@brad-aisa.com>
2008-06-25 22:01:11 +0000

    added bench scripts

---
+++ bench/thread/run_leabra_bench_4.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+# this is a script file for running various standard configurations of
+# nb_thread using same params as leabra_bench.proj
+# use this file for 2-core systems
+
+# <n_units> <n_cycles>  <n_procs>  <n_lays>  <n_cons>(=def) <send_act>  
+
+# Receiver-based
+
+./nb_thread 25 10000 0 5 0 100 -algo=0 -header
+./nb_thread 25 10000 1 5 0 100 -algo=0 
+./nb_thread 25 10000 2 5 0 100 -algo=0
+./nb_thread 25 10000 4 5 0 100 -algo=0
+
+./nb_thread 100 2000 0 5 0 100 -algo=0
+./nb_thread 100 2000 1 5 0 100 -algo=0 
+./nb_thread 100 2000 2 5 0 100 -algo=0
+./nb_thread 100 2000 4 5 0 100 -algo=0
+
+./nb_thread 625 100 0 5 0 100 -algo=0
+./nb_thread 625 100 1 5 0 100 -algo=0 
+./nb_thread 625 100 2 5 0 100 -algo=0
+./nb_thread 625 100 4 5 0 100 -algo=0
+
+./nb_thread 1024 50 0 5 0 100 -algo=0
+./nb_thread 1024 50 1 5 0 100 -algo=0 
+./nb_thread 1024 50 2 5 0 100 -algo=0
+./nb_thread 1024 50 4 5 0 100 -algo=0
+
+
+# Sender-based, clobber
+
+./nb_thread 25 10000 0 5 0 5 -algo=2
+./nb_thread 25 10000 1 5 0 5 -algo=2 
+./nb_thread 25 10000 2 5 0 5 -algo=2
+./nb_thread 25 10000 4 5 0 5 -algo=2
+
+./nb_thread 100 2000 0 5 0 5 -algo=2
+./nb_thread 100 2000 1 5 0 5 -algo=2 
+./nb_thread 100 2000 2 5 0 5 -algo=2
+./nb_thread 100 2000 4 5 0 5 -algo=2
+
+./nb_thread 625 100 0 5 0 5 -algo=2
+./nb_thread 625 100 1 5 0 5 -algo=2 
+./nb_thread 625 100 2 5 0 5 -algo=2
+./nb_thread 625 100 4 5 0 5 -algo=2
+
+./nb_thread 1024 50 0 5 0 5 -algo=2
+./nb_thread 1024 50 1 5 0 5 -algo=2 
+./nb_thread 1024 50 2 5 0 5 -algo=2
+./nb_thread 1024 50 4 5 0 5 -algo=2
+
+
+# Sender-based, array
+
+./nb_thread 25 10000 0 5 0 5 -algo=3
+./nb_thread 25 10000 1 5 0 5 -algo=3 
+./nb_thread 25 10000 2 5 0 5 -algo=3
+./nb_thread 25 10000 4 5 0 5 -algo=3
+
+./nb_thread 100 2000 0 5 0 5 -algo=3
+./nb_thread 100 2000 1 5 0 5 -algo=3 
+./nb_thread 100 2000 2 5 0 5 -algo=3
+./nb_thread 100 2000 4 5 0 5 -algo=3
+
+./nb_thread 625 100 0 5 0 5 -algo=3
+./nb_thread 625 100 1 5 0 5 -algo=3 
+./nb_thread 625 100 2 5 0 5 -algo=3
+./nb_thread 625 100 4 5 0 5 -algo=3
+
+./nb_thread 1024 50 0 5 0 5 -algo=3
+./nb_thread 1024 50 1 5 0 5 -algo=3 
+./nb_thread 1024 50 2 5 0 5 -algo=3
+./nb_thread 1024 50 4 5 0 5 -algo=3
