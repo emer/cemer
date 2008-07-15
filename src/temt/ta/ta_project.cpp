7ed7ee41585056a8ea898141be4583d4a2997a2a
Brad Aisa <baisa@brad-aisa.com>
2008-07-15 05:37:43 +0000

    patched bad ref to root on shutdown

--- src/temt/ta/ta_project.cpp
+++ src/temt/ta/ta_project.cpp
@@ -2066,6 +2066,9 @@ void taRootBase::Cleanup_Main() {
   if (milestone & SM_REG_SIG) {
     taMisc::Register_Cleanup(SIG_DFL); // replace back to using default
   }
+//TEMP: prolly better to have cssMisc::Destroy to keep adding anything else to
+  // since cssMiscHardVars had root pushed on it, make sure it is released
+  cssMisc::HardVars.Reset();
   if (milestone & SM_ROOT_CREATE)
     tabMisc::DeleteRoot();
   if (milestone & SM_TYPES_INIT)
