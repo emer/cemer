fbffa8a7aef25272f7ecf29fe13a9f74fbaedcb4
Randall O'Reilly <oreilly@ucdavis.edu>
2008-07-25 06:44:12 +0000

    oops forgot new files.

---
+++ src/temt/taiqtso/idialog.cpp
@@ -0,0 +1,36 @@
+// Copyright, 1995-2007, Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+//
+// This file is part of The Emergent Toolkit
+//
+//   This library is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU Lesser General Public
+//   License as published by the Free Software Foundation; either
+//   version 2.1 of the License, or (at your option) any later version.
+//   
+//   This library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   Lesser General Public License for more details.
+
+#include "idialog.h"
+
+iDialog::iDialog(QWidget* parent, Qt::WindowFlags f) : QDialog(parent, f) {
+}
+
+void iDialog::keyPressEvent(QKeyEvent *e) {
+  // support Ctrl-Return/Enter as Accept
+  bool ctrl_pressed = false;
+  if(e->modifiers() & Qt::ControlModifier)
+    ctrl_pressed = true;
+#ifdef TA_OS_MAC
+  // ctrl = meta on apple
+  if(e->modifiers() & Qt::MetaModifier)
+    ctrl_pressed = true;
+#endif
+  if(ctrl_pressed && (e->key() == Qt::Key_Enter || e->key() == Qt::Key_Return)) {
+    accept();
+    return;
+  }
+  QDialog::keyPressEvent(e);
+}
---
+++ src/temt/taiqtso/idialog.h
@@ -0,0 +1,33 @@
+// Copyright, 1995-2007, Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+//
+// This file is part of The Emergent Toolkit
+//
+//   This library is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU Lesser General Public
+//   License as published by the Free Software Foundation; either
+//   version 2.1 of the License, or (at your option) any later version.
+//   
+//   This library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   Lesser General Public License for more details.
+
+#ifndef IDIALOG_H
+#define IDIALOG_H
+
+#include "taiqtso_def.h"
+
+#include <QDialog>
+
+class iDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+  explicit iDialog(QWidget *parent = 0, Qt::WindowFlags f = 0);
+protected:
+    void keyPressEvent(QKeyEvent *);
+};
+
+#endif // IDIALOG_H
