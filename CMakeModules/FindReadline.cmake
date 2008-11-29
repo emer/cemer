3eedabecc64359b6b768d7aacf46f2f5b5ea5b65
Randall O'Reilly <oreilly@ucdavis.edu>
2008-11-29 00:42:07 +0000

    suffixes and cleanup

---
+++ CMakeModules/FindReadline.cmake
@@ -0,0 +1,23 @@
+# find Readline (terminal input library) includes and library
+#
+# READLINE_INCLUDE_DIR - where the directory containing the READLINE headers can be found
+# READLINE_LIBRARY     - full path to the READLINE library
+# READLINE_FOUND       - TRUE if READLINE was found
+
+FIND_PATH(READLINE_INCLUDE_DIR readline/readline.h
+        /usr/include
+        /usr/local/include
+	/opt/local/include
+    $ENV{INCLUDE}
+)
+
+FIND_LIBRARY(READLINE_LIBRARY NAMES readline PATH
+   /usr/lib
+   /usr/local/lib
+   /opt/local/lib
+) 
+
+# handle the QUIETLY and REQUIRED arguments and set COIN_FOUND to TRUE if 
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Readline DEFAULT_MSG READLINE_LIBRARY READLINE_INCLUDE_DIR)
--- CMakeModules/MacroLibrary.cmake
+++ CMakeModules/MacroLibrary.cmake
@@ -1,7 +1,7 @@
 # this is a library of useful macros for use throughout the build process
 
 # add path to each of the files in the file list, and return result in first arg
-# IMPORTANT: surround the _file_list arg in double quotes to get this to work!
+# IMPORTANT: surround the _file_list arg in double quotes when calling!
 
 MACRO (ADD_PATH_TO_FILES _result _path _file_list)
    SET(${_result})
@@ -9,4 +9,13 @@ MACRO (ADD_PATH_TO_FILES _result _path _file_list)
       SET(${_result} ${${_result}} "${_path}/${_current}")
    ENDFOREACH(_current ${_file_list})
 ENDMACRO(ADD_PATH_TO_FILES)
+
+# use this instead of TARGET_LINK_LIBRARIES -- sets suffix and other properties
+MACRO (EMERGENT_LINK_LIBRARIES _targ)
+    target_link_libraries(${_targ} ${EMERGENT_LIBRARIES})
+    set_target_properties(${_targ} PROPERTIES
+      VERSION ${EMERGENT_LIB_VERSION}
+      SOVERSION ${EMERGENT_LIB_SOVERSION}
+    )
+ENDMACRO (EMERGENT_LINK_LIBRARIES)
 
