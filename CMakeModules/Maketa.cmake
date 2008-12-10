90b5629ee6e46f75e3217e84e7c5e0e2f496ef94
Brad Aisa <baisa@brad-aisa.com>
2008-12-10 04:34:52 +0000

    collapsed Maketa for all

--- CMakeModules/Maketa.cmake
+++ CMakeModules/Maketa.cmake
@@ -11,75 +11,26 @@ MACRO (MAKETA_GET_INC_DIRS _MAKETA_INC_DIRS)
 ENDMACRO(MAKETA_GET_INC_DIRS)
 
 if (WIN32)
-SET(MAKETA_FLAGS -css -hx -win_dll)
-
-# this is critical for allowing dependencies to work out, and for compiling w/out extra load
-MACRO (SET_TA_PROPS _ta_name _path)
-  SET_SOURCE_FILES_PROPERTIES(${_path}/${_ta_name}_TA.cpp
-    PROPERTIES COMPILE_FLAGS "/Od" GENERATED 1    
-    )
-ENDMACRO(SET_TA_PROPS)
-
-MACRO (CREATE_MAKETA_COMMAND _ta_name _path _maketa_headers)
-  MAKETA_GET_INC_DIRS(maketa_includes)
+  SET(MAKETA_FLAGS -css -hx -win_dll)
   
-  SET(pta "${_path}/${_ta_name}")
-  
-if (false)  
-  ADD_CUSTOM_COMMAND (
-    OUTPUT ${pta}_TA.ccx ${pta}_TA_type.hx ${pta}_TA_inst.hx 
-    COMMAND maketa ${MAKETA_FLAGS} ${maketa_includes} ${_ta_name} ${_maketa_headers}
-    WORKING_DIRECTORY ${_path}
-    DEPENDS maketa
-    MAIN_DEPENDENCY ${_maketa_headers}
-  )
-  ADD_CUSTOM_COMMAND (
-    OUTPUT ${pta}_TA.cpp ${pta}_TA_type.h ${pta}_TA_inst.h 
-    COMMAND maketa ${MAKETA_FLAGS} ${maketa_includes} ${_ta_name} ${_maketa_headers}
-    WORKING_DIRECTORY ${_path}
-    DEPENDS maketa
-    MAIN_DEPENDENCY ${_maketa_headers}
-  )
-endif (false)  
-  ADD_CUSTOM_COMMAND(
-    OUTPUT ${pta}_TA.ccx ${pta}_TA_type.hx ${pta}_TA_inst.hx
-    WORKING_DIRECTORY ${_path}
-    COMMAND maketa ${MAKETA_FLAGS} ${maketa_includes} ${_ta_name} ${_maketa_headers}
-    DEPENDS ${_maketa_headers}
-    )
-
-  ADD_CUSTOM_COMMAND(
-    OUTPUT ${pta}_TA.cpp ${pta}_TA_type.h ${pta}_TA_inst.h 
-    WORKING_DIRECTORY ${_path}
-    COMMAND ${CMAKE_COMMAND} -E compare_files ${pta}_TA.ccx  ${pta}_TA.cpp || ${CMAKE_COMMAND} -E copy  ${pta}_TA.ccx  ${pta}_TA.cpp
-    COMMAND ${CMAKE_COMMAND} -E compare_files ${pta}_TA_type.hx  ${pta}_TA_type.h || ${CMAKE_COMMAND} -E copy  ${pta}_TA_type.hx  ${pta}_TA_type.h
-    COMMAND ${CMAKE_COMMAND} -E compare_files  ${pta}_TA_inst.hx  ${pta}_TA_inst.h || ${CMAKE_COMMAND} -E copy  ${pta}_TA_inst.hx  ${pta}_TA_inst.h
-    DEPENDS ${pta}_TA.ccx ${pta}_TA_type.hx ${pta}_TA_inst.hx
-    )
-  
-  
-  ADD_CUSTOM_TARGET(${_ta_name}_TA.cpp DEPENDS ${_maketa_headers})
-
-  # this target is so that other targets can depend on us
-#  ADD_CUSTOM_TARGET (${_ta_name}_TA DEPENDS ${pta}_TA.cpp)
-
-  ADD_CUSTOM_TARGET(force_ta_${_ta_name} maketa ${MAKETA_FLAGS} -autohx ${maketa_includes} ${_ta_name} ${_maketa_headers}
-    WORKING_DIRECTORY ${_path}
-  )
-
-  SET_TA_PROPS(${_ta_name} ${_path})
-ENDMACRO (CREATE_MAKETA_COMMAND)
+  # this is critical for allowing dependencies to work out, and for compiling w/out extra load
+  MACRO (SET_TA_PROPS _ta_name _path)
+    SET_SOURCE_FILES_PROPERTIES(${_path}/${_ta_name}_TA.cpp
+      PROPERTIES COMPILE_FLAGS "/Od" GENERATED 1    
+      )
+  ENDMACRO(SET_TA_PROPS)
 
 else (WIN32)
 
-# TODO: add a find cpp thing
-SET(MAKETA_FLAGS -hx -css -cpp=\"g++ -E\")
-
-MACRO (SET_TA_PROPS _ta_name _path)
-  SET_SOURCE_FILES_PROPERTIES(${_path}/${_ta_name}_TA.cpp
-    PROPERTIES COMPILE_FLAGS "-O0 -g0" GENERATED 1    
-    )
-ENDMACRO(SET_TA_PROPS)
+  # TODO: add a find cpp thing
+  SET(MAKETA_FLAGS -hx -css -cpp=\"g++ -E\")
+  
+  MACRO (SET_TA_PROPS _ta_name _path)
+    SET_SOURCE_FILES_PROPERTIES(${_path}/${_ta_name}_TA.cpp
+      PROPERTIES COMPILE_FLAGS "-O0 -g0" GENERATED 1    
+      )
+  ENDMACRO(SET_TA_PROPS)
+endif (WIN32)
 
 MACRO (CREATE_MAKETA_COMMAND _ta_name _path _maketa_headers)
   MAKETA_GET_INC_DIRS(maketa_includes)
@@ -92,20 +43,23 @@ MACRO (CREATE_MAKETA_COMMAND _ta_name _path _maketa_headers)
     COMMAND maketa ${MAKETA_FLAGS} ${maketa_includes} ${_ta_name} ${_maketa_headers}
     DEPENDS ${_maketa_headers}
     )
-
+  # note: don't concat these commands, not tested on Windows
+  # note: only use cmake -E commands, must work on Unix and Windows
   ADD_CUSTOM_COMMAND(
     OUTPUT ${pta}_TA.cpp ${pta}_TA_type.h ${pta}_TA_inst.h 
     WORKING_DIRECTORY ${_path}
-    COMMAND cmp -s ${pta}_TA.ccx  ${pta}_TA.cpp || cp  ${pta}_TA.ccx  ${pta}_TA.cpp\; cmp -s ${pta}_TA_type.hx  ${pta}_TA_type.h || cp  ${pta}_TA_type.hx  ${pta}_TA_type.h\; cmp -s ${pta}_TA_inst.hx  ${pta}_TA_inst.h || cp  ${pta}_TA_inst.hx  ${pta}_TA_inst.h
+    COMMAND ${CMAKE_COMMAND} -E compare_files ${pta}_TA.ccx  ${pta}_TA.cpp || ${CMAKE_COMMAND} -E copy  ${pta}_TA.ccx  ${pta}_TA.cpp
+    COMMAND ${CMAKE_COMMAND} -E compare_files ${pta}_TA_type.hx  ${pta}_TA_type.h || ${CMAKE_COMMAND} -E copy  ${pta}_TA_type.hx  ${pta}_TA_type.h
+    COMMAND ${CMAKE_COMMAND} -E compare_files  ${pta}_TA_inst.hx  ${pta}_TA_inst.h || ${CMAKE_COMMAND} -E copy  ${pta}_TA_inst.hx  ${pta}_TA_inst.h
     DEPENDS ${pta}_TA.ccx ${pta}_TA_type.hx ${pta}_TA_inst.hx
     )
-
-  ADD_CUSTOM_TARGET(${_ta_name}_TA.cpp DEPENDS ${_maketa_headers})
+  
+#nn  ADD_CUSTOM_TARGET(${_ta_name}_TA.cpp DEPENDS ${_maketa_headers})
 
   # this target is so that other targets can depend on us
-  ADD_CUSTOM_TARGET (${_ta_name}_TA
-    DEPENDS ${pta}_TA.cpp
-  )
+#nn  ADD_CUSTOM_TARGET (${_ta_name}_TA
+#nn    DEPENDS ${pta}_TA.cpp
+#nn  )
 
   ADD_CUSTOM_TARGET(force_ta_${_ta_name}
     COMMAND maketa ${MAKETA_FLAGS} -autohx ${maketa_includes} ${_ta_name} ${_maketa_headers}
@@ -119,4 +73,3 @@ MACRO (CREATE_MAKETA_COMMAND _ta_name _path _maketa_headers)
   SET_TA_PROPS(${_ta_name} ${_path})
 ENDMACRO (CREATE_MAKETA_COMMAND)
 
-endif (WIN32)
