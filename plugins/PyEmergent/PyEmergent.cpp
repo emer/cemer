1add12e49a14101d051e6552668b7bf2c53f8164
Brian Mingus <mingus@grey.colorado.edu>
2009-03-28 09:56:16 +0000

    see wiki page: http://grey.colorado.edu/emergent/index.php/PyEmergent

--- plugins/PyEmergent/PyEmergent.cpp
+++ plugins/PyEmergent/PyEmergent.cpp
@@ -1,40 +1,29 @@
+/////////////////////////////////////////////////////
+//  PyEmergent
+/////////////////////////////////////////////////////
+
 #include </usr/include/python2.5/Python.h>
 #include "PyEmergent.h"
-
 #include "ta_project.h"
 
-/////////////////////////////////////////////////////
-//  PyemergentPluginExampleClass
-/////////////////////////////////////////////////////
-
-void PyemergentPluginExampleClass::Initialize() {
-// TODO: delete following and add your own member initializations
-  module_name = "time";
-  function_name = "time";
-}
-
-void PyemergentPluginExampleClass::Destroy() {
-  CutLinks(); // unlinks any owned objects -- always put this in Destroy
-  // TODO: release any manually allocated resources, if any
-}
-
-void PyemergentPluginExampleClass::UpdateAfterEdit_impl() {
-  // by using the 'inherited' pseudo-keyword we always call the right base class
-  inherited::UpdateAfterEdit_impl();
-//TODO: delete following lines, add any update code you may have, or leave empty
-  // we always keep sum_a_b up to date
-  //  sum_a_b = a + b;
-}
-
-
-//TODO: replace the methods below with your own methods
-void PyemergentPluginExampleClass::Hello() {
-  //taMisc::Info("Hello from PyemergentPluginExampleClass!");
-  PyObject *pName, *pModule, *pFunc;
-  PyObject *pArgs, *pValue;
+void PyEmergent::Initialize() {
   Py_Initialize();
+}
+
+void PyEmergent::Destroy() {
+  CutLinks();
+  Py_Finalize();
+}
+
+void PyEmergent::UpdateAfterEdit_impl() {
+  inherited::UpdateAfterEdit_impl();
+}
+
+Variant PyEmergent::CallPythonFunNoArgs(String module_name, String function_name) {
+  PyObject *pName, *pModule, *pFunc;
+  PyObject *pValue;
+
   pName = PyString_FromString(module_name);
-    /* Error checking of pName left out */
   pModule = PyImport_Import(pName);
   Py_DECREF(pName);
 
@@ -43,20 +32,7 @@ void PyemergentPluginExampleClass::Hello() {
     /* pFunc is a new reference */
 
     if (pFunc && PyCallable_Check(pFunc)) {
-      pArgs = PyTuple_New(argc - 3);
-      for (i = 0; i < argc - 3; ++i) {
-	pValue = PyInt_FromLong(atoi(argv[i + 3]));
-	if (!pValue) {
-	  Py_DECREF(pArgs);
-	  Py_DECREF(pModule);
-	  taMisc::Info("Cannot convert argument");
-	  return;
-	}
-	/* pValue reference stolen here: */
-	PyTuple_SetItem(pArgs, i, pValue);
-      }
-      pValue = PyObject_CallObject(pFunc, pArgs);
-      Py_DECREF(pArgs);
+      pValue = PyObject_CallObject(pFunc, NULL);
       if (pValue != NULL) {
 	printf("%ld\n", PyInt_AsLong(pValue));
 	Py_DECREF(pValue);
@@ -65,36 +41,26 @@ void PyemergentPluginExampleClass::Hello() {
 	Py_DECREF(pFunc);
 	Py_DECREF(pModule);
 	PyErr_Print();
-	taMisc::Info("Call failed\n");
-	return;
+	taMisc::Info("Call failed");
+	return 1;
 
       }
     }
     else {
       if (PyErr_Occurred())
 	PyErr_Print();
-      taMisc::Info("Cannot find function" + function_name);
+      taMisc::Info("Cannot find function: " + function_name);
     }
     Py_XDECREF(pFunc);
     Py_DECREF(pModule);
   }
   else {
     PyErr_Print();
-    taMisc::Info("Failed to load" + module_name);
-    return;
+    taMisc::Info("Failed to load: " + module_name);
+    return 1;
   }
-  Py_Finalize();
-  return;
-}
 
-void PyemergentPluginExampleClass::AddToAandB(int add_to_a, int add_to_b) {
-//   a += add_to_a;
-//   b += add_to_b;
-  UpdateAfterEdit(); // updates any associated members, refreshes display, and notifies clients of this class
-}
-
-void PyemergentPluginExampleClass::MyCodeMethod() {
-   // example, does nothing
+  return 0;
 }
 
 /////////////////////////////////////////////////////
@@ -110,7 +76,6 @@ PyemergentPluginState* PyemergentPluginState::instance() {
 
 void PyemergentPluginState::Initialize() {
   SetBaseFlag(NAME_READONLY); // usually don't let user edit name
-// TODO: add your member initializations
 }
 
 void PyemergentPluginState::Destroy() {
@@ -119,8 +84,5 @@ void PyemergentPluginState::Destroy() {
 }
 
 void PyemergentPluginState::UpdateAfterEdit_impl() {
-  // by using the 'inherited' pseudo-keyword we always call the right base class
   inherited::UpdateAfterEdit_impl();
-//TODO: add any update code you may have, or leave empty
 }
-
--- plugins/PyEmergent/PyEmergent.h
+++ plugins/PyEmergent/PyEmergent.h
@@ -2,69 +2,33 @@
 #define PYEMERGENT_H
 
 #include "PyEmergent_def.h"
-#include "ta_base.h" // TODO: replace with the temt/emergent file that contains your base class
-//TODO: add any headers of additional types that are used in your class, such as ta_geometry.h 
+#include "ta_base.h"
 
-
-class PYEMERGENT_API PyemergentPluginExampleClass : public taNBase
-{ // TODO: describe your class
-  INHERITED(taNBase) // declares 'inherited' keyword for safer base-class references
+class PYEMERGENT_API PyEmergent : public taNBase
+{
+  INHERITED(taNBase)
 public:
-  String		example_member1; // TODO: replace these and add your own members
-  Variant		example_member2; // a variant
-  int			a; // a first number
-  int			b; // a second number
-  int			sum_a_b; // #READ_ONLY #NO_SAVE a third number, only accessible by the program
-  int argc;
-  char *argv[0];  
   String module_name;
+
   String function_name;
-  int i;
+  Variant 			CallPythonFunNoArgs(String module_name, String function_name); // #MENU Hello Function!
 
-  // TODO: delete the following text and methods, and start adding your methods here
-  // Comment directives such as the ones below are explained here:
-  // http://grey.colorado.edu/emergent/index.php/Comment_directives
-  // This particular directive creates a menu for the function Hello() in an edit dialog
-  void 			Hello(); // #MENU Hello Function!
-  // The next method declares a button in the dialog
-  void 			AddToAandB(int add_to_a, int add_to_b); 
-  // #BUTTON adds amounts to a and b and signals a change
-  void			MyCodeMethod(); // example of a method that will be used by your code, but not available to the user in a menu -- it will be availabe in the CSS scripting language
-
-
-  SIMPLE_LINKS(PyemergentPluginExampleClass) // automatically links embedded taBase objects into ownership chain
-  TA_BASEFUNS(PyemergentPluginExampleClass) // defines a default set of constructors and copy code 
+  SIMPLE_LINKS(PyEmergent)
+  TA_BASEFUNS(PyEmergent)
 
 protected:
-  // use the 'override' pseudo-keyword to indicate overridden virtual methods
   override void		UpdateAfterEdit_impl(); // called when a class changes, just prior to gui updates and client class notifications
 private:
-  SIMPLE_COPY(PyemergentPluginExampleClass) // enables this object to be copied
+  SIMPLE_COPY(PyEmergent) // enables this object to be copied
   void	Initialize();
   void	Destroy();
-}; //
+};
 
-/* PyemergentPluginState
-
-  This class is used to hold user options and internal state. An instance
-  is automatically created in the .root.plugin_state collections.
-  Note: if you rename your plugin you MUST fix up this class name accordingly.
-*/
 class PYEMERGENT_API PyemergentPluginState : public taFBase
-{ // this class is used to hold and save/restore user options and internal state for the PyemergentPlugin 
+{
   INHERITED(taFBase)
 public:
   static PyemergentPluginState* instance(); 
-  // use this accessor routine to obtain your instance
-  
-/* TODO: add user options or internal state here and delete this comment block
-  -- examples of how to do some are below
-   
-   String	user_option1; // this text here will explain to the user what "user_option1" does
-   int		user_status1; // #READ_ONLY #NO_SAVE this item will display for user as read-only, and won't get saved
-   float	internal_state1; // #HIDDEN the user won't see this, but it will still be saved/loaded
-*/
-  
   SIMPLE_LINKS(PyemergentPluginState)
   TA_BASEFUNS(PyemergentPluginState)
 protected:
@@ -74,5 +38,4 @@ private:
   void	Initialize();
   void	Destroy();
 };
-
 #endif
