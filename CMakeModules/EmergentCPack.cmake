aef19cda22a4a69068bd4f8ae237568b36f7b4d2
Randall O'Reilly <oreilly@ucdavis.edu>
2009-12-04 10:24:54 +0000

    windows cpack all done -- works great

---
+++ CMakeModules/EmergentCPack.cmake
@@ -0,0 +1,42 @@
+# This file contains all of our cpack configuration information, which is then generated out to CPackConfig.cmake
+# DO NOT EDIT CPackConfig.cmake directly!!!
+
+SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "The Emergent Neural Simulation System")
+SET(CPACK_PACKAGE_VENDOR "Computational Cognitive Neuroscience Lab, University of Colorado at Boulder")
+SET(CPACK_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README")
+SET(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/COPYING")
+SET(CPACK_PACKAGE_VERSION_MAJOR "${EMERGENT_VERSION_MAJOR}")
+SET(CPACK_PACKAGE_VERSION_MINOR "${EMERGENT_VERSION_MINOR}")
+SET(CPACK_PACKAGE_VERSION_PATCH "${EMERGENT_VERSION_PATCH}")
+
+if (WIN32)
+# There is a bug in NSI that does not handle full unix paths properly. Make
+# sure there is at least one set of four (4) backlasshes.
+  SET(CPACK_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}/src/temt/ta/images\\\\emergent_32x32.png")
+  SET(CPACK_NSIS_MUI_ICON "${CMAKE_CURRENT_SOURCE_DIR}/src/temt/ta/images\\\\emergent_32x32.ico")
+  SET(CPACK_PACKAGE_FILE_NAME "emergent-${EMERGENT_VERSION}-win32")
+  SET(CPACK_NSIS_MODIFY_PATH ON)
+else (WIN32)
+  SET(CPACK_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}/src/temt/ta/images/emergent_32x32.png")
+endif (WIN32)
+
+SET(CPACK_PACKAGE_EXECUTABLES "emergent" "Emergent")
+INCLUDE(CPack)
+
+# following are currently unused
+#SET(CPACK_BINARY_BUNDLE "")
+#SET(CPACK_BINARY_CYGWIN "")
+#SET(CPACK_BINARY_DEB "ON")
+#SET(CPACK_BINARY_NSIS "OFF")
+#SET(CPACK_BINARY_OSXX11 "")
+#SET(CPACK_BINARY_PACKAGEMAKER "")
+#SET(CPACK_BINARY_RPM "OFF")
+#SET(CPACK_BINARY_STGZ "OFF")
+#SET(CPACK_BINARY_TBZ2 "OFF")
+#SET(CPACK_BINARY_TGZ "OFF")
+#SET(CPACK_BINARY_TZ "OFF")
+#SET(CPACK_BINARY_ZIP "OFF")
+#SET(CPACK_COMPONENTS_ALL "")
+#SET(CPACK_COMPONENT_UNSPECIFIED_HIDDEN "TRUE")
+#SET(CPACK_COMPONENT_UNSPECIFIED_REQUIRED "TRUE")
+#SET(CPACK_DEBIAN_PACKAGE_DEPENDS "subversion, cmake, g++, libqt4-dev, libcoin60-dev, libreadline5-dev, libgsl0-dev, libode0-dev, libquarter")
