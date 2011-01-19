a3a1908a075f0937c8d754b5b4203036bd3d710c
Dan Furlani <dpfurlani@grey.colorado.edu>
2011-01-19 01:20:16 +0000

    Bug 566: fixed installer icons and desktop shortcut

--- CMakeModules/EmergentCPack.cmake
+++ CMakeModules/EmergentCPack.cmake
@@ -4,19 +4,44 @@
 SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "The Emergent Neural Simulation System")
 SET(CPACK_PACKAGE_VENDOR "Computational Cognitive Neuroscience Lab, University of Colorado at Boulder")
 SET(CPACK_PACKAGE_CONTACT "emergent-users@grey.colorado.edu")
-SET(CPACK_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README.txt")
+SET(CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README.txt")
 SET(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/COPYING.txt")
 SET(CPACK_PACKAGE_VERSION_MAJOR "${EMERGENT_VERSION_MAJOR}")
 SET(CPACK_PACKAGE_VERSION_MINOR "${EMERGENT_VERSION_MINOR}")
 SET(CPACK_PACKAGE_VERSION_PATCH "${EMERGENT_VERSION_PATCH}")
 
 if (WIN32)
-# There is a bug in NSI that does not handle full unix paths properly. 
-# Make sure there is at least one set of four (4) backlasshes.
-  SET(CPACK_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}/src/temt/ta/images\\\\emergent.ico")
-  SET(CPACK_NSIS_MUI_ICON "${CMAKE_CURRENT_SOURCE_DIR}/src/temt/ta/images\\\\emergent_16x16x256.ico")
+  # There is a bug in NSIS that does not handle full unix paths properly.
+  # Make sure there is at least one set of four (4) backslashes.
+  
+  # Set the name of the installer executable to be created.
   SET(CPACK_PACKAGE_FILE_NAME "emergent-${EMERGENT_VERSION}-win32")
+  
+  # Set the icon used for the installer (and uninstaller).
+  SET(CPACK_NSIS_MUI_ICON "${CMAKE_CURRENT_SOURCE_DIR}/src/temt/ta/images\\\\emergent.ico")
+  SET(CPACK_NSIS_MUI_UNIICON "${CMAKE_CURRENT_SOURCE_DIR}/src/temt/ta/images\\\\emergent.ico")
+  
+  # Set a graphic to use in the installer (not an icon).
+  SET(CPACK_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}/src/temt/ta/images\\\\emergent-install.bmp")
+  
+  # Set the icon used in the Add/Remove Programs control panel.
+  # The path has to be relative to the install directory, not
+  # the build/source directory.  The file has to be an .exe.
+  SET(CPACK_NSIS_INSTALLED_ICON_NAME "bin\\\\emergent${EMERGENT_SUFFIX}.exe")
+  
+  # Have the installer prompt the user with the "Install Options" screen,
+  # which has prompts to modify the system path and create desktop shortcuts.
   SET(CPACK_NSIS_MODIFY_PATH ON)
+  
+  # Set the list of desktop shortcuts to create, if the user so chooses.
+  # However, if the user later selects "Do not create shortcuts" on the
+  # "Choose Start Menu Folder" screen, it not only omits the SM shortcuts,
+  # but also skips the desktop shortcut.  This is probably a bug in NSIS.
+  #
+  # Has to be lowercase "emergent", not "Emergent" to match what's in
+  # CPACK_PACKAGE_EXECUTABLES.
+  SET(CPACK_CREATE_DESKTOP_LINKS emergent)
+  
 else (WIN32)
   # Replace CMAKE_INSTALL_PREFIX to Currently used one,
   # If it wasn't overridden from command line / cache.
