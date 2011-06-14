8ba293a1047166e34b2c86428e851696e1071146
Randall O'Reilly <oreilly@ucdavis.edu>
2011-06-14 18:32:16 +0000

    exclude taBase on css type import too -- already there -- thanks Dan!

--- src/temt/css/css_builtin.cpp
+++ src/temt/css/css_builtin.cpp
@@ -2139,7 +2139,8 @@ static void Install_Types() {
       else if(!((tmp->members.size==0) && (tmp->methods.size==0)) &&
 	      (tmp->name != "fstream") && (tmp->name != "class") &&
 	      (tmp->name != "taString") && (tmp->name != "taLeafItr") &&
-	      (tmp->name != "sstream") && (tmp->name != "Variant"))
+	      (tmp->name != "sstream") && (tmp->name != "Variant") &&
+	      (tmp->name != "taBase"))
       {
 	if(tmp->InheritsFrom(TA_taBase)) {
 	  taBase* inst = (taBase*)tmp->GetInstance();
