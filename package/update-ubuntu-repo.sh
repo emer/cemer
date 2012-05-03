ebf90b21b577141482cb1edc751f10ee8024f0db
Dan Furlani <dpfurlani@grey.colorado.edu>
2012-05-03 22:41:33 +0000

    updated ubuntu repo update script to take an optional list of repos to update, otherwise defaults to the 'latest' repo

--- package/update-ubuntu-repo.sh
+++ package/update-ubuntu-repo.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Loads emergent and libquarter .deb files from your home directory
 # into the repositories.  Run as root.
+# Usage:
+#   ubuntu-repo-update.sh             # Update the latest repo
+#   ubuntu-repo-update.sh main        # Update the main repo
+#   ubuntu-repo-update.sh main latest # Update the main and latest repos
+
 RUN_ON=grey
 if ! grep -q "$RUN_ON" /etc/hostname; then
   echo "ERROR: This script should be run on ${RUN_ON}"
@@ -14,27 +19,52 @@ fi
 
 CWD=`pwd`
 DIST_ROOT=/usr/local/ubuntu
-# Don't mess with the 'main' repositories, those should be stable
-# for months at a time.  Make a new repo called "latest".
-REPO=latest
-for DIST in lucid maverick natty oneiric; do
-  for ARCH in i386 amd64; do
-    echo "Moving ${ARCH} files ..."
-    SUBDIR=dists/${DIST}/${REPO}/binary-${ARCH}
-    mkdir -p ${DIST_ROOT}/${SUBDIR}
-    cd ${DIST_ROOT}
-    for PACKAGE in emergent libquarter; do
-      FILE=${PACKAGE}*${ARCH}.deb
-      FILEPATH=${CWD}/${DIST}/${FILE}
-      echo "  Moving ${FILEPATH} to ${SUBDIR} ..."
-      mv ${FILEPATH} ${SUBDIR}
-      chmod 644 ${SUBDIR}/${FILE}
-      chown root.root ${SUBDIR}/${FILE}
+
+# By default, only update the 'latest' repo.  Or, if a repo was given on
+# the command line (such as 'main'), or multiple repos were given (such as
+# 'latest main'), then update that repo(s) instead.
+# Generally the main repo is only updated once per semester.
+REPOS=latest
+if [ -n "$1" ]; then
+  REPOS="$1"
+fi
+
+for REPO in $REPOS; do
+  for DIST in lucid maverick natty oneiric; do
+    for ARCH in i386 amd64; do
+      echo "Copying ${ARCH} files ..."
+      SUBDIR=dists/${DIST}/${REPO}/binary-${ARCH}
+      mkdir -p ${DIST_ROOT}/${SUBDIR}
+      cd ${DIST_ROOT}
+      for PACKAGE in emergent libquarter; do
+        FILE=${PACKAGE}*${ARCH}.deb
+        FILEPATH=${CWD}/${DIST}/${FILE}
+        echo "  Copying ${FILEPATH} to ${SUBDIR} ..."
+        cp ${FILEPATH} ${SUBDIR}
+        chmod 644 ${SUBDIR}/${FILE}
+        chown root.root ${SUBDIR}/${FILE}
+      done
+      echo "  Updating ${ARCH} repository ..."
+      dpkg-scanpackages ${SUBDIR} /dev/null | grep -v -E 'Depends:[^a-z]$' > ${SUBDIR}/Packages
+      gzip -f ${SUBDIR}/Packages
     done
-    echo "  Updating ${ARCH} repository ..."
-    dpkg-scanpackages ${SUBDIR} /dev/null | grep -v -E 'Depends:[^a-z]$' > ${SUBDIR}/Packages
-    gzip -f ${SUBDIR}/Packages
   done
 done
 
 echo -e "\n\nNOTE: Warnings about 'override file' are expected\n"
+
+echo -e "\n\nUpdated repos: ${REPOS}"
+echo "Press enter to delete original .deb files,"
+read -p "or Ctrl-C if another repo needs to be updated " JUNK
+
+# Now that all repos have been updated, delete the files.
+for DIST in lucid maverick natty oneiric; do
+  for ARCH in i386 amd64; do
+    for PACKAGE in emergent libquarter; do
+      FILE=${PACKAGE}*${ARCH}.deb
+      FILEPATH=${CWD}/${DIST}/${FILE}
+      echo "  Deleting ${FILEPATH}..."
+      rm ${FILEPATH}
+    done
+  done
+done
