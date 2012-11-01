310caa1ba28b07b97f9140131de42ee6d88b52df
Houman Farokhzad <hfarokhzad@grey.colorado.edu>
2012-11-01 04:05:35 +0000

    an example of the subversion c api - compiles but not functional yet

---
+++ subversion_example/subversion/Debug/src/subversion.d
@@ -0,0 +1,129 @@
+src/subversion.d: ../src/subversion.cpp /usr/include/apr-1.0/apr.h \
+ /usr/include/apr-1.0/apr_xlate.h /usr/include/apr-1.0/apu.h \
+ /usr/include/apr-1.0/apr_pools.h /usr/include/apr-1.0/apr.h \
+ /usr/include/apr-1.0/apr_errno.h /usr/include/apr-1.0/apr_general.h \
+ /usr/include/apr-1.0/apr_want.h /usr/include/apr-1.0/apr_allocator.h \
+ /usr/include/apr-1.0/apr_thread_mutex.h /usr/include/apr-1.0/apr_hash.h \
+ /usr/include/apr-1.0/apr_errno.h /usr/include/apr-1.0/apr_pools.h \
+ /usr/include/apr-1.0/apr_file_io.h /usr/include/apr-1.0/apr_time.h \
+ /usr/include/apr-1.0/apr_file_info.h /usr/include/apr-1.0/apr_user.h \
+ /usr/include/apr-1.0/apr_tables.h /usr/include/apr-1.0/apr_inherit.h \
+ /usr/include/apr-1.0/apr_general.h \
+ /usr/include/subversion-1/svn_client.h /usr/include/apr-1.0/apr_tables.h \
+ /usr/include/apr-1.0/apr_getopt.h /usr/include/apr-1.0/apr_time.h \
+ /usr/include/subversion-1/svn_types.h /usr/include/apr-1.0/apr_strings.h \
+ /usr/include/subversion-1/svn_error.h /usr/include/apr-1.0/apr_want.h \
+ /usr/include/subversion-1/svn_error_codes.h \
+ /usr/include/subversion-1/svn_string.h \
+ /usr/include/subversion-1/svn_wc.h \
+ /usr/include/subversion-1/svn_checksum.h \
+ /usr/include/subversion-1/svn_io.h /usr/include/apr-1.0/apr_file_info.h \
+ /usr/include/apr-1.0/apr_thread_proc.h \
+ /usr/include/apr-1.0/apr_file_io.h /usr/include/subversion-1/svn_delta.h \
+ /usr/include/subversion-1/svn_version.h \
+ /usr/include/subversion-1/svn_opt.h /usr/include/subversion-1/svn_ra.h \
+ /usr/include/subversion-1/svn_auth.h \
+ /usr/include/subversion-1/svn_config.h \
+ /usr/include/subversion-1/svn_mergeinfo.h \
+ /usr/include/subversion-1/svn_diff.h \
+ /usr/include/subversion-1/svn_repos.h /usr/include/subversion-1/svn_fs.h \
+ /usr/include/subversion-1/svn_fs.h /usr/include/subversion-1/svn_pools.h \
+ /usr/include/subversion-1/svn_error.h
+
+/usr/include/apr-1.0/apr.h:
+
+/usr/include/apr-1.0/apr_xlate.h:
+
+/usr/include/apr-1.0/apu.h:
+
+/usr/include/apr-1.0/apr_pools.h:
+
+/usr/include/apr-1.0/apr.h:
+
+/usr/include/apr-1.0/apr_errno.h:
+
+/usr/include/apr-1.0/apr_general.h:
+
+/usr/include/apr-1.0/apr_want.h:
+
+/usr/include/apr-1.0/apr_allocator.h:
+
+/usr/include/apr-1.0/apr_thread_mutex.h:
+
+/usr/include/apr-1.0/apr_hash.h:
+
+/usr/include/apr-1.0/apr_errno.h:
+
+/usr/include/apr-1.0/apr_pools.h:
+
+/usr/include/apr-1.0/apr_file_io.h:
+
+/usr/include/apr-1.0/apr_time.h:
+
+/usr/include/apr-1.0/apr_file_info.h:
+
+/usr/include/apr-1.0/apr_user.h:
+
+/usr/include/apr-1.0/apr_tables.h:
+
+/usr/include/apr-1.0/apr_inherit.h:
+
+/usr/include/apr-1.0/apr_general.h:
+
+/usr/include/subversion-1/svn_client.h:
+
+/usr/include/apr-1.0/apr_tables.h:
+
+/usr/include/apr-1.0/apr_getopt.h:
+
+/usr/include/apr-1.0/apr_time.h:
+
+/usr/include/subversion-1/svn_types.h:
+
+/usr/include/apr-1.0/apr_strings.h:
+
+/usr/include/subversion-1/svn_error.h:
+
+/usr/include/apr-1.0/apr_want.h:
+
+/usr/include/subversion-1/svn_error_codes.h:
+
+/usr/include/subversion-1/svn_string.h:
+
+/usr/include/subversion-1/svn_wc.h:
+
+/usr/include/subversion-1/svn_checksum.h:
+
+/usr/include/subversion-1/svn_io.h:
+
+/usr/include/apr-1.0/apr_file_info.h:
+
+/usr/include/apr-1.0/apr_thread_proc.h:
+
+/usr/include/apr-1.0/apr_file_io.h:
+
+/usr/include/subversion-1/svn_delta.h:
+
+/usr/include/subversion-1/svn_version.h:
+
+/usr/include/subversion-1/svn_opt.h:
+
+/usr/include/subversion-1/svn_ra.h:
+
+/usr/include/subversion-1/svn_auth.h:
+
+/usr/include/subversion-1/svn_config.h:
+
+/usr/include/subversion-1/svn_mergeinfo.h:
+
+/usr/include/subversion-1/svn_diff.h:
+
+/usr/include/subversion-1/svn_repos.h:
+
+/usr/include/subversion-1/svn_fs.h:
+
+/usr/include/subversion-1/svn_fs.h:
+
+/usr/include/subversion-1/svn_pools.h:
+
+/usr/include/subversion-1/svn_error.h:
