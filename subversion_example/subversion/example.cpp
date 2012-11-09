29fd09a4a159d292a8781e55759e75aeadba723c
Dan Furlani <dpfurlani@grey.colorado.edu>
2012-11-09 18:52:52 +0000

    reviewed and updated code * don't use tabs.  indent width is 2 spaces. * checkout() func should take params: url, path, revision * C structs are not C++ classes, don't have constructors, need to initialze them!  e.g., svn_opt_revision_t * doesn't make sense to 'new' C structs.  APR uses its own memory allocator (pools).  structs can be created on the stack if only needed for the duration of a svn C API function call. * probably doesn't make sense to canonicalize a URL. * comments and TODOs * need to figure out "context" stuff so we can scrap the kdesvncpp code.  i'm not convinced it will work to mix & match our own impl with theirs. * don't copy&paste API docs into calling code.  It's OK as a reminder when you're trying to config all the arguments to pass to the function but scrap it once you figure that out. * check for error after C API call and report

--- subversion_example/subversion/example.cpp
+++ subversion_example/subversion/example.cpp
@@ -18,71 +18,114 @@
 
 #include <iostream>
 
-int testSvnMakeDir() {
-	/*	old stuff
-	 apr_initialize();
-	 apr_pool_t * pool = svn_pool_create(0);
-	 const char * path = svn_path_canonicalize("/home/houman/Desktop/wc/", pool);
-	 const char * newDir = svn_path_dirname(svn_path_canonicalize("dir3", pool),
-	 pool);
-	 //make_new_directory(path, newDir, pool);
-	 svn_commit_info_t ** commit_info_p = svn_create_commit_info(pool);
-	 const apr_array_header_t * paths = "/home/houman/Desktop/wc/";
-	 //svn_client_ctx_t * ctx = svn_client_create_context();
-	 //svn_client_mkdir(commit_info_p, paths, ctx, pool);
-	 svn_pool_destroy(pool);
-	 apr_terminate();
-	 */
-	return 0;
+int testSvnMakeDir()
+{
+  /*  old stuff
+   apr_initialize();
+   apr_pool_t * pool = svn_pool_create(0);
+   const char * path = svn_path_canonicalize("/home/houman/Desktop/wc/", pool);
+   const char * newDir = svn_path_dirname(svn_path_canonicalize("dir3", pool),
+   pool);
+   //make_new_directory(path, newDir, pool);
+   svn_commit_info_t ** commit_info_p = svn_create_commit_info(pool);
+   const apr_array_header_t * paths = "/home/houman/Desktop/wc/";
+   //svn_client_ctx_t * ctx = svn_client_create_context();
+   //svn_client_mkdir(commit_info_p, paths, ctx, pool);
+   svn_pool_destroy(pool);
+   apr_terminate();
+   */
+  return 0;
 }
 
-int testSvnCheckOut() {
-	apr_initialize();
-	apr_pool_t * pool = svn_pool_create(0);
+int checkout(const char *url, const char *working_copy_path, int rev = -1)
+{
+  // Set up APR and a pool -- eventually we'll move this somewhere
+  // more global.
+  apr_initialize();
+  apr_pool_t * pool = svn_pool_create(0);
 
-	svn_revnum_t * result_rev = new svn_revnum_t();
-	const char * URL = svn_path_canonicalize("file:///home/houman/Desktop/svn",
-			pool);
-	const char * path = svn_path_canonicalize("/home/houman/Desktop/", pool);
-	const svn_opt_revision_t * peg_revision = new svn_opt_revision_t();
-	const svn_opt_revision_t * revision = new svn_opt_revision_t();
-	svn_boolean_t ignore_externals = false;
-	svn_boolean_t allow_unver_obstructions = true;
-	svn::Context * context = new svn::Context();
-	svn_client_ctx_t * ctx = context->ctx();
-	try {
-		/*
-		 * Checkout a working copy from a repository.
-		 * Parameters:
-		 * [out]result_rev
-		 * 		If non-NULL, the value of the revision checked out from the repository.
-		 * [in]	URL	The repository URL of the checkout source.
-		 * [in]	path	The root of the new working copy.
-		 * [in]	peg_revision	The peg revision.
-		 * [in]	revision	The operative revision.
-		 * [in]	depth	The depth of the operation. If svn_depth_unknown, then behave as if for svn_depth_infinity, except in the case of resuming a previous checkout of path (i.e., updating), in which case use the depth of the existing working copy.
-		 * [in]	ignore_externals
-		 * 		If TRUE, don't process externals definitions as part of this operation.
-		 * [in]	allow_unver_obstructions
-		 * 		If TRUE, then tolerate existing unversioned items that obstruct incoming paths. Only obstructions of the same type (file or dir) as the added item are tolerated. The text of obstructing files is left as-is, effectively treating it as a user modification after the checkout. Working properties of obstructing items are set equal to the base properties.
-		 * 		If FALSE, then abort if there are any unversioned obstructing items.
-		 * [in]	ctx	The standard client context, used for authentication and notification.
-		 * [in]	pool	Used for any temporary allocation.
-		 */
-		svn_error_t * err = svn_client_checkout3(result_rev, URL, path,
-				peg_revision, revision, svn_depth_infinity, ignore_externals,
-				allow_unver_obstructions, ctx, pool);
-		//std::cout << err->message << std::endl;
-	} catch (svn::ClientException &e) {
-		std::cout << e.message() << std::endl;
-	}
-	svn_pool_destroy(pool);
-	apr_terminate();
+  // Out parameter -- the value of the revision checked out from the
+  // repository.
+  svn_revnum_t result_rev;
+
+  // URL isn't a path.  Even if the repo is on local disk and accessed
+  // with file:///, that's still a URL, not a path.
+  //url = svn_path_canonicalize(url, pool);
+
+  // Canonicalize the path.
+  working_copy_path = svn_path_canonicalize(working_copy_path, pool);
+
+  // We don't want to use peg revisions, so set to unspecified.
+  svn_opt_revision_t peg_revision;
+  peg_revision.kind = svn_opt_revision_unspecified;
+
+  // Set the revision number, if provided.  Otherwise get HEAD revision.
+  svn_opt_revision_t revision;
+  if (rev < 0) {
+    revision.kind = svn_opt_revision_head;
+  }
+  else {
+    revision.kind = svn_opt_revision_number;
+    revision.value.number = rev;
+  }
+
+  // Get all files.
+  svn_depth_t depth = svn_depth_infinity;
+
+  // Set advanced options we don't care about.
+  svn_boolean_t ignore_externals = false;
+  svn_boolean_t allow_unver_obstructions = true;
+
+  // TODO: Are we going to use kdesvncpp stuff or roll our own?
+  // If we're just using this as a way to get an initialized value for
+  // svn_client_ctx_t then that's not good.
+  svn::Context * context = new svn::Context();
+  svn_client_ctx_t * ctx = context->ctx();
+
+  // Don't need to wrap in try/catch because it's just a C call.
+  // (C doesn't have exceptions).  Although, if the callbacks to
+  // whatever svn::Context provides throw exceptions then we have
+  // a problem.  Let's see if we can avoid using kdesvncpp directly.
+  try {
+    svn_error_t * err =
+      svn_client_checkout3(
+        &result_rev, // out param
+        url,
+        working_copy_path,
+        peg_revision,
+        revision,
+        depth,
+        ignore_externals,
+        allow_unver_obstructions,
+        ctx,
+        pool);
+
+    // Check for error.
+    if (err) {
+      // TODO: Hook this into taMisc::Warning()
+      std::cout << "Subversion error: " << err->message << std::endl;
+      svn_error_clear(err);
+      return -1;
+    }
+  }
+  catch (svn::ClientException &e) {
+    std::cout << e.message() << std::endl;
+  }
+
+  // Clean up the pool and APR context.
+  // TODO: this should be more global.
+  svn_pool_destroy(pool);
+  apr_terminate();
 }
 
-int main() {
-	//testSvnMakeDir();
-	testSvnCheckOut();
-	std::cout << "DONE!";
-	return 0;
+int main()
+{
+  //testSvnMakeDir();
+
+  testSvnCheckOut(
+    "http://grey.colorado.edu/svn/emergent/emergent/trunk/package",
+    "/home/houman/Desktop/");
+
+  std::cout << "DONE!";
+  return 0;
 }
