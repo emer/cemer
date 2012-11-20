9c41388f7a317de78b7a71a15984c7345448757f
Dan Furlani <dpfurlani@grey.colorado.edu>
2012-11-20 19:59:01 +0000

    copy and paste Update(), MakeDir(), Checkin() function bodies from subversion_example/subversion/emergent_svn/subversion.cpp

--- src/temt/ta/Subversion.cpp
+++ src/temt/ta/Subversion.cpp
@@ -416,8 +416,60 @@ int
 Subversion::Update(int rev)
 {
   m_cancelled = false;
-  // TODO.
-  return -1;
+
+// TODO: rest of function copied from example dir, needs cleanup.
+
+  // Out parameter -- the value of the revision checked out from the repository.
+  apr_array_header_t *result_revs;
+
+  // create an array containing a single element which is the input path to be updated
+  apr_array_header_t *paths = apr_array_make(pool.pool(), 1, sizeof(const char *));
+  path = svn_path_canonicalize(path, pool.pool());
+  (*((const char **) apr_array_push(paths))) = path;
+
+  // Set the revision number, if provided. Otherwise get HEAD revision.
+  svn_opt_revision_t revision;
+  if (rev < 0) {
+    revision.kind = svn_opt_revision_head;
+  } else {
+    revision.kind = svn_opt_revision_number;
+    revision.value.number = rev;
+  }
+
+  // Get all files.
+  svn_depth_t depth = svn_depth_infinity;
+
+  // Set advanced options we don't care about.
+  svn_boolean_t ignore_externals = false;
+  svn_boolean_t allow_unver_obstructions = true;
+  svn_boolean_t depth_is_sticky = true;
+
+  // TODO: implement our own context
+  svn::Context context;
+  svn_client_ctx_t *ctx = context.ctx();
+
+  svn_error_t *err = svn_client_update3(&result_revs, paths, &revision, depth, depth_is_sticky, ignore_externals,
+      allow_unver_obstructions, ctx, pool.pool());
+
+  // Check for error.
+  if (err) {
+    // TODO: Hook this into taMisc::Warning()
+    std::cout << "Subversion error: " << err->message << std::endl;
+    svn_error_clear(err);
+    return false;
+  }
+
+  /*  the following loop provides a vector containing all updated revision numbers
+   *  I commented it out since we don't need it.
+   std::vector<svn_revnum_t> rev_nums;
+   int i;
+   for (i = 0; i < result_revs->nelts; i++) {
+   svn_revnum_t rev_num = APR_ARRAY_IDX(result_revs, i, svn_revnum_t) ;
+   rev_nums.push_back(rev_num);
+   }
+   */
+
+  return true;
 }
 
 int
@@ -432,8 +484,39 @@ bool
 Subversion::MakeDir(const char *new_dir, bool create_parents)
 {
   m_cancelled = false;
-  // TODO.
-  return false;
+
+// TODO: rest of function copied from example dir, needs cleanup.
+
+  // won't be used unless we make an immediate commit after adding files (by setting revprop_table)
+  svn_commit_info_t *commit_info_p = svn_create_commit_info(pool.pool());
+
+  // create an array containing a single path to be created
+  apr_array_header_t *paths = apr_array_make(pool.pool(), 1, sizeof(const char *));
+  path = svn_path_canonicalize(path, pool.pool());
+  (*((const char **) apr_array_push(paths))) = path;
+
+  // create any non-existent parent directories
+  svn_boolean_t make_parents = true;
+
+  // we need to set revprop_table to a non-null if we wanna make an immediate commit after adding files
+  // svn_client_propget3 can be used to create an apr_hash_t
+  const apr_hash_t *revprop_table = NULL;
+
+  // TODO: implement our own context
+  svn::Context context;
+  svn_client_ctx_t *ctx = context.ctx();
+
+  svn_error_t *err = svn_client_mkdir3(&commit_info_p, paths, make_parents, revprop_table, ctx, pool.pool());
+
+  // Check for error.
+  if (err) {
+    // TODO: Hook this into taMisc::Warning()
+    std::cout << "Subversion error: " << err->message << std::endl;
+    svn_error_clear(err);
+    return false;
+  }
+
+  return true;
 }
 
 bool
@@ -450,8 +533,51 @@ Subversion::Checkin(const char *comment, const char *files)
   m_cancelled = false;
   // 'files' is a comma or newline separated list of files and/or directories.
   // If empty, the whole working copy will be committed.
-  // TODO.
-  return -1;
+
+// TODO: rest of function copied from example dir, needs cleanup.
+
+  //svn_commit_info_t *commit_info_p = svn_create_commit_info(pool.pool());
+  svn_commit_info_t *commit_info_p = NULL;
+
+  // create an array containing a single element which is the path path to be committed
+  apr_array_header_t *paths = apr_array_make(pool.pool(), 1, sizeof(const char *));
+  path = svn_path_canonicalize(path, pool.pool());
+  (*((const char **) apr_array_push(paths))) = path;
+
+  // commit changes to the children of the paths
+  svn_depth_t depth = svn_depth_infinity;
+
+  // unlock paths in the repository after commit
+  svn_boolean_t keep_locks = false;
+
+  // no need to changelist filtering
+  svn_boolean_t keep_changelists = false;
+  const apr_array_header_t *changelists = apr_array_make(pool.pool(), 0, sizeof(const char *));
+
+  const apr_hash_t *revprop_table = NULL;
+
+  // TODO: implement our own context
+  svn::Context context;
+  context.setLogMessage("message");
+  svn_client_ctx_t *ctx = context.ctx();
+
+
+  // to be used if we need to load the log message from a file
+  //const char *tmp_file = NULL;
+  //(*svn_client_get_commit_log3_t)(&message, tmp_file, paths, ctx->log_msg_baton3, pool.pool());
+
+  svn_error_t *err = svn_client_commit4(&commit_info_p, paths, depth, keep_locks, keep_changelists, changelists,
+      revprop_table, ctx, pool.pool());
+
+  // Check for error.
+  if (err) {
+    // TODO: Hook this into taMisc::Warning()
+    std::cout << "Subversion error: " << err->message << std::endl;
+    svn_error_clear(err);
+
+    return false;
+  }
+  return 1; // TODO: return the new revision
 }
 
 int
