56a9ae62c85cf26fd702fc13c3172d8051c86cb7
Randall O'Reilly <oreilly@ucdavis.edu>
2013-05-02 22:14:21 +0000

    first pass on cerebellum granule cell

---
+++ src/emergent/leabra/CerebGranuleUnitSpec.cpp
@@ -0,0 +1,74 @@
+// Copyright, 1995-2013, Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+//
+// This file is part of Emergent
+//
+//   Emergent is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   Emergent is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+#include "CerebGranuleUnitSpec.h"
+#include <LeabraUnit>
+
+
+void CerebGranuleSpecs::Initialize() {
+  act_thr = 0.5f;
+  inhib_start_time = 10;
+  lrn_start_time = 120;
+  lrn_end_time = 150;
+}
+
+void CerebGranuleUnitSpec::Compute_NetinInteg(LeabraUnit* u, LeabraNetwork* net,
+                                           int       thread_no) {
+  inherited::Compute_NetinInteg(u, net, thread_no);
+  int time_since_thr = (int)u->net_ctxt;
+  if(time_since_thr > cereb.inhib_start_time) {
+    u->net = 0.0f;
+    u->i_thr = 0.0f;
+  }
+}
+
+void CerebGranuleUnitSpec::Compute_GranLearnAct(LeabraUnit* u, LeabraNetwork* net,
+                                                int thread_no) {
+  int time_since_thr = (int)u->net_ctxt;
+  if(time_since_thr == 0) {
+    u->act_lrn = 0.0f;
+    if(u->act > cereb.act_thr) {
+      time_since_thr = 1;
+      u->act_ctxt = u->act;     // current act max
+    }
+    else {
+      u->act_ctxt = 0.0f;       // reset max always
+    }
+  }
+  else {      // if 0, we have not crossed threshold
+    time_since_thr++;
+    if(time_since_thr < cereb.inhib_start_time) {
+      u->act_ctxt = MAX(u->act_ctxt, u->act); // get max within time window
+      u->act_lrn = 0.0f;        // no learning yet
+    }
+    else if(time_since_thr < cereb.lrn_start_time) {
+      u->act_lrn = 0.0f;        // no learning yet
+    }
+    else if(time_since_thr <= cereb.lrn_end_time) {
+      u->act_lrn = u->act_ctxt; // set learning to previous max
+    }
+    else {                      // at end of time window
+      time_since_thr = 0;       // reset everything
+      u->act_ctxt = 0.0f;
+      u->act_lrn = 0.0f;
+    }
+  }
+  u->net_ctxt = time_since_thr; // update counter
+}
+
+void CerebGranuleUnitSpec::Compute_Act(Unit* u, Network* net, int thread_no) {
+  inherited::Compute_Act(u, net, thread_no);
+  Compute_GranLearnAct((LeabraUnit*)u, (LeabraNetwork*)net, thread_no);
+}
---
+++ src/emergent/leabra/CerebGranuleUnitSpec.h
@@ -0,0 +1,80 @@
+// Copyright, 1995-2013, Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+//
+// This file is part of Emergent
+//
+//   Emergent is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   Emergent is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+#ifndef CerebGranuleUnitSpec_h
+#define CerebGranuleUnitSpec_h 1
+
+// parent includes:
+#include <LeabraUnitSpec>
+
+// member includes:
+
+// declare all other types mentioned but not required to include:
+
+eTypeDef_Of(CerebGranuleSpecs);
+
+class E_API CerebGranuleSpecs : public SpecMemberBase {
+  // ##INLINE ##INLINE_DUMP ##NO_TOKENS #NO_UPDATE_AFTER ##CAT_Leabra parameters for the cerebellar granule unit
+INHERITED(SpecMemberBase)
+public:
+  float         act_thr;        // #DEF_0.5 activation threshold for initiating eligibility for learning and triggering the inhibitory window
+  int           inhib_start_time; // #DEF_10 how long after unit crosses act_thr do we start inhibiting
+  int           lrn_start_time;  // #DEF_120 starting time window (in cycles) for learning -- how long after activity crosses act_thr threshold does the learning window start?
+  int           lrn_end_time;    // #DEF_150 ending time window (in cycles) for learning -- the learning window closes after this time
+
+  override String       GetTypeDecoKey() const { return "UnitSpec"; }
+
+  TA_SIMPLE_BASEFUNS(CerebGranuleSpecs);
+protected:
+  SPEC_DEFAULTS;
+  //  void	UpdateAfterEdit_impl();
+private:
+  void	Initialize();
+  void	Destroy()	{ };
+  void	Defaults_init();
+};
+
+// notes on use of LeabraUnit variables for the Granule unit:
+// act_lrn = time-lagged learning activation value -- this is what Purkinje cells use to learn from
+// net_ctxt = start time when unit crosses act_thr threshold activation
+// act_ctxt = max activation of unit within activity window
+
+// UnitSpec functions:
+// in Compute_NetinInteg or Compute_IThresh -- negate net input for inhiitory competition during "inhibitory refractory period"
+// in Compute_Act or a sub-function, we implement act_lrn etc.
+
+
+eTypeDef_Of(CerebGranuleUnitSpec);
+
+class E_API CerebGranuleUnitSpec : public LeabraUnitSpec {
+  // cerebellum granule unit -- enforces phasic temporal activity and provides lagged learning signals
+INHERITED(LeabraUnitSpec)
+public:
+  CerebGranuleSpecs     cereb;  // specs for cerebellar granule units
+
+  virtual void	Compute_GranLearnAct(LeabraUnit* u, LeabraNetwork* net,
+                                     int thread_no=-1);
+  // compute the granule unit learning activation as a function of time
+
+  override void	Compute_NetinInteg(LeabraUnit* u, LeabraNetwork* net, int       thread_no=-1);
+  override void	Compute_Act(Unit* u, Network* net, int thread_no=-1);
+
+  TA_SIMPLE_BASEFUNS(CerebGranuleUnitSpec);
+private:
+  void Initialize()  { };
+  void Destroy()     { };
+};
+
+#endif // CerebGranuleUnitSpec_h
