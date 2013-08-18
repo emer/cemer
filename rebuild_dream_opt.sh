a05b2152981bd5a2e5854ee60ae807ef2228dc58
Randall O'Reilly <oreilly@ucdavis.edu>
2013-08-18 07:26:31 +0000

    rebuild script fix

--- rebuild_dream_opt.sh
+++ rebuild_dream_opt.sh
@@ -3,4 +3,4 @@
 ssh -t dream "cd emergent/build; svn up ../; make -j4; make -j4; sudo make install"
 
 # this version will rebuild plugins with last call
-ssh -t dream "cd emergent/build; svn up ../; make -j4; make -j4; sudo make install; emergent -nogui -ni"
+#ssh -t dream "cd emergent/build; svn up ../; make -j4; make -j4; sudo make install; emergent -nogui -ni"
