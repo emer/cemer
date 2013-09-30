0ae348d71b0735f7c4ed8053166d7cf8d2eb5fcf
John Rohrlich <john.rohrlich@colorado.edu>
2013-09-30 22:56:23 +0000

    Near fix for bug 1523 - Hitting enter on LocalVars inserts new line of code, but should create a local var

--- src/temt/ta/iTreeView.cpp
+++ src/temt/ta/iTreeView.cpp
@@ -26,6 +26,10 @@
 #include <taiObjectMimeFactory>
 #include <iBrowseHistory>
 #include <iVec2i>
+#include <ProgVar>
+#include <ProgVar_List>
+#include <Program>
+#include <LocalVars>
 
 #include <SigLinkSignal>
 #include <taMisc>
@@ -39,11 +43,7 @@
 #include <QScrollBar>
 #include <QKeyEvent>
 
-
-
-
 const String iTreeView::opt_treefilt("TREEFILT_");
-
 
 void iTreeView::FillTypedList(const QList<QTreeWidgetItem*>& items,
   ISelectable_PtrList& list)
@@ -273,26 +273,53 @@ void iTreeView::InsertEl(bool after) {
 }
 
 void iTreeView::InsertDefaultEl(bool after) {
+
+  bool doit = true;
+  ProgVar* pv = NULL;
+
   ISelectable* si = curItem();
-  if(!si || !si->link()) return;                // nothing selected
+  if(!si || !si->link())
+    return;                // nothing selected
   taBase* sb = si->link()->taData();
   if(!sb) return;
   taList_impl* sbo = NULL;
   if(sb->InheritsFrom(&TA_taList_impl)) {
     sbo = (taList_impl*)sb;
   }
-  else {
-    sbo = GET_OWNER(sb, taList_impl);
+  else {  // not a list
+    if(sb->InheritsFrom(&TA_ProgEl)) {
+      TypeDef* sb_td = sb->GetTypeDef();
+      String mbr = sb_td->OptionAfter("DEF_CHILD_");
+      if (!mbr.empty() && mbr == "local_vars") {
+        // these 2 lines put the new var in the "program vars" list
+//        Program* pgm = GET_OWNER(sb, Program);
+//        sbo = &pgm->vars;
+//        but we want to add the var to the "local vars" list
+        LocalVars* locals = dynamic_cast<ProgEl*>(sb)->FindLocalVarList();
+        if (locals) {
+          sbo = &locals->local_vars;  // the list of local vars
+        }
+      }
+      else {
+        sbo = GET_OWNER(sb, taList_impl);
+      }
+    }
+    else {
+      sbo = GET_OWNER(sb, taList_impl);
+    }
   }
   if(!sbo) return;
   if(sbo->HasOption("FIXED_SIZE")) return; // cannot manipulate in gui
   TypeDef* td = sbo->el_typ;    // default type
+
   if(td) {
     taProject* proj = myProject();
     if(proj) {
       proj->undo_mgr.SaveUndo(sbo, "InsertDefaultEl", NULL, false, sbo); // global save
     }
+
     taBase* nwi = taBase::MakeToken(td);
+
     if(nwi) {
       int idx = 0;
       if(sbo == sb) {           // it is the list
