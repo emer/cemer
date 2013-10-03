067808a3cd67c7c17eca0b0c0b51f8e5b9c0f0bb
Randall O'Reilly <oreilly@ucdavis.edu>
2013-10-03 07:56:27 +0000

    have to list one at a time -- missed these

--- DartConfiguration.tcl
+++ DartConfiguration.tcl
@@ -4,38 +4,32 @@
 
 
 # Configuration directories and files
-SourceDirectory: /Users/thazy/emergent
-BuildDirectory: /Users/thazy/emergent
-
-# Where to place the cost data store
-CostDataFile: 
+SourceDirectory: /Users/mingus/emergent
+BuildDirectory: /Users/mingus/emergent
 
 # Site is something like machine.domain, i.e. pragmatic.crd
-Site: macbookpro.local
+Site: mightyme.local
 
 # Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
 BuildName: Darwin-c++
 
 # Submission information
 IsCDash: TRUE
-CDashVersion: 
-QueryCDashVersion: 
 DropSite: grey.colorado.edu
 DropLocation: /CDash/submit.php?project=Emergent
 DropSiteUser: 
 DropSitePassword: 
 DropSiteMode: 
 DropMethod: http
-TriggerSite: 
+TriggerSite: http://grey.colorado.edu/cgi-bin/Submit-Random-TestingResults.cgi
 ScpCommand: /usr/bin/scp
 
 # Dashboard start time
 NightlyStartTime: 04:00:00 EST
 
 # Commands for the build/test/submit cycle
-ConfigureCommand: "/Applications/CMake 2.8-8.app/Contents/bin/cmake" "/Users/thazy/emergent"
+ConfigureCommand: "/usr/local/bin/cmake" "/Users/mingus/emergent"
 MakeCommand: /usr/bin/make -i
-DefaultCTestConfigurationType: Release
 
 # CVS options
 # Default is "-d -P -A"
@@ -43,51 +37,26 @@ CVSCommand: /usr/bin/cvs
 CVSUpdateOptions: -d -A -P
 
 # Subversion options
-SVNCommand: /opt/local/bin/svn
+SVNCommand: /usr/local/bin/svn
 SVNUpdateOptions: 
 
-# Git options
-GITCommand: /usr/bin/git
-GITUpdateOptions: 
-GITUpdateCustom: 
-
 # Generic update command
-UpdateCommand: /opt/local/bin/svn
+UpdateCommand: /usr/local/bin/svn
 UpdateOptions: 
 UpdateType: svn
 
-# Compiler info
-Compiler: /usr/bin/c++
-
-# Dynamic analysis (MemCheck)
+# Dynamic analisys and coverage
 PurifyCommand: 
 ValgrindCommand: 
 ValgrindCommandOptions: 
 MemoryCheckCommand: MEMORYCHECK_COMMAND-NOTFOUND
 MemoryCheckCommandOptions: 
 MemoryCheckSuppressionFile: 
-
-# Coverage
 CoverageCommand: /usr/bin/gcov
-CoverageExtraFlags: -l
-
-# Cluster commands
-SlurmBatchCommand: SLURM_SBATCH_COMMAND-NOTFOUND
-SlurmRunCommand: SLURM_SRUN_COMMAND-NOTFOUND
 
 # Testing options
 # TimeOut is the amount of time in seconds to wait for processes
 # to complete during testing.  After TimeOut seconds, the
-# process will be summarily terminated.
+# process will be summaily terminated.
 # Currently set to 25 minutes
 TimeOut: 1500
-
-UseLaunchers: 
-CurlOptions: 
-# warning, if you add new options here that have to do with submit,
-# you have to update cmCTestSubmitCommand.cxx
-
-# For CTest submissions that timeout, these options
-# specify behavior for retrying the submission
-CTestSubmitRetryDelay: 5
-CTestSubmitRetryCount: 3
