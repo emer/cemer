500537032743e635178f7f1211afd83a4203e6e6
Randall O'Reilly <oreilly@ucdavis.edu>
2014-04-14 23:07:32 +0000

    Join as synonym for ToDelimString

--- src/temt/ta/String_Array.h
+++ src/temt/ta/String_Array.h
@@ -39,11 +39,15 @@ public:
 
   String        ToDelimString(const String& delim = " ");
   // generates a string of all the items on the list, using given delimiter between items
+  String        Join(const String& delim = " ")
+  { return ToDelimString(delim); }
+  // generates a string of all the items on the list, using given delimiter between items (same as ToDelimString)
   void          FmDelimString(const String& str, const String& delim = " ", bool reset_first = true);
   // add strings to this array by parsing given string using given delimiter separating strings -- reset first = reset this array before adding (else append)
 
   void          Split(const String& str, const String& delim = "");
   // Convenience method that calls FmDelimString for splitting a string into an array. Splits on every character by default.
+
 
   TA_BASEFUNS(String_Array);
   TA_ARRAY_FUNS(String_Array, String)
