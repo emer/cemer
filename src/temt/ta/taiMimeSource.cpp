a9ca14fdb690ed009ba44ebb2d22983aeaf8338a
Randall O'Reilly <oreilly@ucdavis.edu>
2014-06-13 06:08:38 +0000

    fixed program_group moving, per guess in bug ticket: issue was Program_TopGroup..

--- src/temt/ta/taiMimeSource.cpp
+++ src/temt/ta/taiMimeSource.cpp
@@ -866,11 +866,8 @@ void taGroup_impl::ChildQueryEditActionsG_impl(const MemberDef* md,
   } else {
     forbidden |= iClipData::EA_IN_PROC_OPS; // note: redundant during queries, but needed for G action calls
   }
-//NOTE: relaxed, 10/14/08 by BA
-  // generic group paste only allows exact type, so we check for each inheriting from the other, which means same
   TypeDef* td = ms->td();
-//old  bool right_gptype = (td && (td->InheritsFrom(GetTypeDef()) && GetTypeDef()->InheritsFrom(td)));
-  bool right_gptype = (td && (td->InheritsFrom(GetTypeDef())) );
+  bool right_gptype = (td && (td->InheritsFrom(gp.el_base)) ); // only base type of subgps is what matters
 
   // find relevant ops 
   int op = 0;
