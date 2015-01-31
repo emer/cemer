5debd2ffe0aabdc518ea6adec47c4f44f85099db
John Rohrlich <john.rohrlich@colorado.edu>
2015-01-31 00:29:00 +0000

    Add "(Alt + Mouse Click to see weight for any unit)"

--- src/emergent/network/Connection.h
+++ src/emergent/network/Connection.h
@@ -38,7 +38,7 @@ eTypeDef_Of(Connection);
 class E_API Connection {
   // ##NO_TOKENS ##NO_UPDATE_AFTER ##CAT_Network base connection between two units
 public:
-  float        wt;             // #VIEW_HOT synaptic weight of connection
+  float        wt;             // #VIEW_HOT synaptic weight of connection (Alt + Mouse Click to see weight for any unit)
   float        dwt;            // #VIEW_HOT #NO_SAVE change in synaptic weight as computed by learning mechanism
 
   Connection() { wt = dwt = 0.0f; }
