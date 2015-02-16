dc8526dda660d9b192ef63f27109e307c798c40a
Randall O'Reilly <oreilly@ucdavis.edu>
2015-02-16 08:02:31 +0000

    all working, logging removed

--- cluster_run/cluster_run_mon_lib.py
+++ cluster_run/cluster_run_mon_lib.py
@@ -1922,7 +1922,7 @@ class SubversionPoller(object):
             prev_dat_files = prev_dat_files.replace("\t", " ")  # somehow getting tab
             dat_files = all_files[0]
             if dat_files != prev_dat_files:
-                logging.info("dat_files updated to: %s from: %s" % (dat_files, prev_dat_files))
+#                logging.info("dat_files updated to: %s from: %s" % (dat_files, prev_dat_files))
                 self.jobs_running.set_val(row, "dat_files", dat_files)
                 # always get the data files whenever they change -- GetData is obsolete..
                 self._getdata_job_tag(tag)
