d302731ebf62865e03a6575f97b21fc619d2eef0
Randall O'Reilly <oreilly@ucdavis.edu>
2015-09-27 08:26:19 +0000

    added LeabraMultCopyUnitSpec -- same functionality as layerspec from 7.0

---
+++ src/emergent/leabra/LeabraMultCopyUnitSpec.cpp
@@ -0,0 +1,77 @@
+// Copyright, 1995-2013, Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+//
+// This file is part of Emergent
+//
+//   Emergent is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   Emergent is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+#include "LeabraMultCopyUnitSpec.h"
+
+#include <LeabraNetwork>
+
+TA_BASEFUNS_CTORS_DEFN(LeabraMultCopyUnitSpec);
+
+void LeabraMultCopyUnitSpec::Initialize() {
+  one_minus = false;
+  mult_gain = 1.0f;
+}
+
+bool LeabraMultCopyUnitSpec::CheckConfig_Unit(Unit* un, bool quiet) {
+  if(!inherited::CheckConfig_Unit(un, quiet)) return false;
+  LeabraUnit* u = (LeabraUnit*)un;
+  bool rval = true;
+  
+  const int nrg = u->NRecvConGps();
+  if(u->CheckError(nrg != 2, quiet, rval,
+                "leabra mult copy must have exactly 2 recv prjns, first = act to copy, second = act to multiply")) {
+    return false;               // fatal
+  }
+
+  LeabraConGroup* copy_gp = (LeabraConGroup*)u->RecvConGroup(0);
+  if(u->CheckError(copy_gp->size != 1, quiet, rval,
+                "leabra mult copy first prjn (copy act source) must have exactly 1 connection to copy from")) {
+    return false;               // fatal
+  }
+  LeabraConGroup* mult_gp = (LeabraConGroup*)u->RecvConGroup(1);
+  if(u->CheckError(mult_gp->size != 1, quiet, rval,
+                "leabra mult copy second prjn (mult act source) must have exactly 1 connection to get mult act from")) {
+    return false;               // fatal
+  }
+  return rval;
+}
+
+void LeabraMultCopyUnitSpec::Compute_MultCopy(LeabraUnitVars* u, LeabraNetwork* net, int thr_no) {
+  LeabraConGroup* copy_gp = (LeabraConGroup*)u->RecvConGroup(net, thr_no, 0);
+  LeabraConGroup* mult_gp = (LeabraConGroup*)u->RecvConGroup(net, thr_no, 1);
+  
+  LeabraUnitVars* copy_un = (LeabraUnitVars*)copy_gp->UnVars(0,net);
+  LeabraUnitVars* mult_un = (LeabraUnitVars*)mult_gp->UnVars(0,net);
+
+  float mult_eff = mult_gain * mult_un->act_eq;;
+  if(mult_eff > 1.0f) mult_eff = 1.0f;
+  float new_act;
+  if(one_minus)
+    new_act = copy_un->act_eq * (1.0f - mult_eff);
+  else
+    new_act = copy_un->act_eq * mult_eff;
+  u->ext = new_act;
+  u->act_eq = u->act_nd = u->act = u->net = u->ext;
+  u->da = 0.0f;               // I'm fully settled!
+}
+                                              
+void LeabraMultCopyUnitSpec::Compute_Act_Rate(LeabraUnitVars* u, LeabraNetwork* net, int thr_no) {
+  Compute_MultCopy(u, net, thr_no);
+}
+
+void LeabraMultCopyUnitSpec::Compute_Act_Spike(LeabraUnitVars* u, LeabraNetwork* net, int thr_no) {
+  Compute_Act_Rate(u, net, thr_no);
+}
+
---
+++ src/emergent/leabra/LeabraMultCopyUnitSpec.h
@@ -0,0 +1,59 @@
+// Copyright, 1995-2013, Regents of the University of Colorado,
+// Carnegie Mellon University, Princeton University.
+//
+// This file is part of Emergent
+//
+//   Emergent is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   Emergent is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+#ifndef LeabraMultCopyUnitSpec_h
+#define LeabraMultCopyUnitSpec_h 1
+
+// parent includes:
+#include <LeabraUnitSpec>
+
+// member includes:
+
+// declare all other types mentioned but not required to include:
+
+eTypeDef_Of(LeabraMultCopyUnitSpec);
+
+class E_API LeabraMultCopyUnitSpec : public LeabraUnitSpec {
+  // unit that copies activations from one layer and multiplies them by values from another -- i.e., multiplicative gating -- must recv from 2 prjns (any more ignored) -- first is copy activation, second is multiplication activation
+INHERITED(LeabraUnitSpec)
+public:
+  bool		one_minus;	// if true, use 1-mult activation as the multiplier -- this is useful with mutually exclusive options in the multipliers, where you hook each up to the *other* alternative, such that this other alternative inhibits this option
+  float		mult_gain;	// multiplier gain -- multiply the mult act value by this gain, with a max overall resulting net mult value of 1
+
+  virtual void	Compute_MultCopy(LeabraUnitVars* u, LeabraNetwork* net, int thr_no);
+  
+  void	Compute_NetinInteg(LeabraUnitVars* u, LeabraNetwork* net, int thr_no) override { };
+  void	Compute_ApplyInhib
+    (LeabraUnitVars* uv, LeabraNetwork* net, int thr_no, LeabraLayerSpec* lspec,
+     LeabraInhib* thr, float ival) override { };
+  void	Compute_Act_Rate(LeabraUnitVars* u, LeabraNetwork* net, int thr_no) override;
+  void	Compute_Act_Spike(LeabraUnitVars* u, LeabraNetwork* net, int thr_no) override;
+
+  void 	Compute_dWt(UnitVars* u, Network* net, int thr_no) override { };
+  void	Compute_dWt_Norm(LeabraUnitVars* u, LeabraNetwork* net, int thr_no) { };
+  void	Compute_Weights(UnitVars* u, Network* net, int thr_no) override { };
+
+  bool  CheckConfig_Unit(Unit* u, bool quiet=false);
+
+  TA_SIMPLE_BASEFUNS(LeabraMultCopyUnitSpec);
+protected:
+  SPEC_DEFAULTS;
+private:
+  void  Initialize();
+  void  Destroy()     { };
+  void  Defaults_init() { Initialize(); }
+};
+
+#endif // LeabraMultCopyUnitSpec_h
