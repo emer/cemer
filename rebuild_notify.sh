c5c0e49bcfd147c9086907ac9c8d08688ccdee9f
Randall O'Reilly <oreilly@ucdavis.edu>
2015-10-09 04:35:05 +0000

    do svn update first -- this runs on grey, which isn't otherwise updated!

--- rebuild_notify.sh
+++ rebuild_notify.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# make sure we've got latest here -- not necc same as target build!
+svn update
+
 svn_rev=`svn info | grep Revision:`
 echo ${svn_rev}
 /usr/bin/mail -s "Emergent on clusters updated to ${svn_rev}" ccnlab-logistics@grey.colorado.edu < /dev/null
