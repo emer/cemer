705ccd27aa0de4f91b9df22878a4438b1974b715
Randall O'Reilly <oreilly@ucdavis.edu>
2015-11-09 22:45:02 +0000

    no deep_mod_net input for units that are not maintaining!

--- src/emergent/leabra/PFCUnitSpec.cpp
+++ src/emergent/leabra/PFCUnitSpec.cpp
@@ -168,7 +168,7 @@ void PFCUnitSpec::UpdateAfterEdit_impl() {
 float PFCUnitSpec::Compute_NetinExtras(LeabraUnitVars* u, LeabraNetwork* net,
                                           int thr_no, float& net_syn) {
   float net_ex = inherited::Compute_NetinExtras(u, net, thr_no, net_syn);
-  if(deep.IsSuper()) {
+  if(deep.IsSuper() && u->thal_cnt > 0.0f) { // only if in maintenance!  not for cleared!
     net_ex += pfc.s_mnt_gain * u->deep_mod_net;
   }
   return net_ex;
@@ -301,8 +301,9 @@ void PFCUnitSpec::ClearOtherMaint(LeabraUnitVars* u, LeabraNetwork* net, int thr
     if(!cs->IsMarkerCon()) continue;
     for(int j=0;j<send_gp->size; j++) {
       LeabraUnitVars* uv = (LeabraUnitVars*)send_gp->UnVars(j,net);
-      if(uv->thal_cnt >= 1.0f)  // important!  only for established maint, not just gated!
+      if(uv->thal_cnt >= 1.0f) { // important!  only for established maint, not just gated!
         uv->thal_cnt = -1.0f; // terminate!
+      }
     }
   }
 }
