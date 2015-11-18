cd390e85d7462992daf39c3fa5bbd3bd59dc8679
Thomas Hazy <thazy@colorado.edu>
2015-11-18 21:14:40 +0000

    added specific else-if for VENTRAL-MATRIX to ComputeDeepMod() in MSNUnitSpec

--- src/emergent/leabra/MSNUnitSpec.cpp
+++ src/emergent/leabra/MSNUnitSpec.cpp
@@ -115,7 +115,26 @@ void MSNUnitSpec::Compute_DeepMod(LeabraUnitVars* u, LeabraNetwork* net, int thr
       }
     }
   }
-  else {
+  else if(dorsal_ventral == VENTRAL && matrix_patch == MATRIX) {
+    // TODO: trying verbatim copy of the BasAmyg version - since want BAext-like behavior
+    LeabraLayer* lay = (LeabraLayer*)u->Un(net, thr_no)->own_lay();
+    if(deep.SendDeepMod()) {
+      u->deep_lrn = u->deep_mod = u->act;      // record what we send!
+    }
+    else if(deep.IsTRC()) {
+      u->deep_lrn = u->deep_mod = 1.0f;         // don't do anything interesting
+    }
+    // must be SUPER units at this point
+    else if(lay->am_deep_mod_net.max < 0.01f) { // not enough yet // was .1f
+      u->deep_lrn = 0.0f;    // default is 0!
+      u->deep_mod = 1.0f;    // TODO: hoping this is the magic guy that bootstraps, but doesn't make totally dependent
+    }
+    else {
+      u->deep_lrn = u->deep_mod_net / lay->am_deep_mod_net.max; // todo: could not normalize this..
+      u->deep_mod = 1.0f;                               // do not modulate with deep_mod!
+    }
+  }
+  else { // must be VENTRAL, PATCH
     inherited::Compute_DeepMod(u, net, thr_no); // use D1D2 one
   }
 }
