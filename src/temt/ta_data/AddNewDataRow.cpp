580c2ba73d2f9b9b38ace100ee75d65bbdd8ce5c
John Rohrlich <john.rohrlich@colorado.edu>
2016-03-27 18:43:09 +0000

    A couple more cases using new CvtFromCodeCheckNames method to check tool bar names and typedef

--- src/temt/ta_data/AddNewDataRow.cpp
+++ src/temt/ta_data/AddNewDataRow.cpp
@@ -45,10 +45,12 @@ void AddNewDataRow::GenCssBody_impl(Program* prog) {
 }
 
 bool AddNewDataRow::CanCvtFmCode(const String& code, ProgEl* scope_el) const {
+  if (CvtFmCodeCheckNames(code))
+    return true;
+  
   String dc = code;  dc.downcase();
   String tbn = GetToolbarName(); tbn.downcase();
   String tn = GetTypeDef()->name; tn.downcase();
-  if(dc.startsWith(tbn) || dc.startsWith(tn)) return true;
   dc.gsub(" ", "");
   if(dc.startsWith("addnew") || dc.startsWith("add_new") ||
      dc.startsWith("newrow") || dc.startsWith("new_row"))
