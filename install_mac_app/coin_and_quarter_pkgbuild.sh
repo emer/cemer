3ae9ead88e4c200db89010c704f0d3ad5c92d227
Randall O'Reilly <oreilly@ucdavis.edu>
2016-04-22 23:02:51 +0000

    updated script -- working now it seems

--- install_mac_app/coin_and_quarter_pkgbuild.sh
+++ install_mac_app/coin_and_quarter_pkgbuild.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-pkgbuild --root /Library/Frameworks/Inventor.framework --identifier coin.emergent.ccnlab.colorado.edu --version 4.0 ./coin_mac64_4_0.pkg
+# note: edit package names here
+coinpkg=coin_mac64_4_0
+qtrpkg=quarter_mac64_qt55
 
-pkgbuild --root /Library/Frameworks/Quarter.framework/ --identifier quarter.emergent.ccnlab.colorado.edu --version 5.5 ./quarter_mac64_qt55.pkg
+pkgbuild --root /Library/Frameworks/Inventor.framework --install-location /Library/Frameworks/Inventor.framework --identifier edu.colorado.ccnlab.emergent.coin --version 4.0 ./${coinpkg}.pkg
 
-#pkgbuild --component /Library/Frameworks/Inventor.framework/ --component /Library/Frameworks/Quarter.framework/ --identifier coin.quarter.emergent.ccnlab.colorado.edu --version 5.5 ./coin_and_quarter_mac64_qt55.pkg
+pkgbuild --root /Library/Frameworks/Quarter.framework --install-location /Library/Frameworks/Quarter.framework --identifier edu.colorado.ccnlab.emergent.quarter --version 5.5 ./${qtrpkg}.pkg
 
-# doesn't like the coin-config -- probably need a script or something.
-#pkgbuild --component /Library/Frameworks/Inventor.framework --component /Library/Frameworks/Quarter.framework --component /usr/local/bin/coin-config --identifier coin.quarter.emergent.ccnlab.colorado.edu --version 5.5 --install-location / ./coin_and_quarter_mac64_qt55.pkg
+./make_dmg_pkg.sh $coinpkg
+./make_dmg_pkg.sh $qtrpkg
 
