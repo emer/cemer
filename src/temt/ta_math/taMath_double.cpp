2be28ec890a33e3803c02c4158f5317328315d3d
John Rohrlich <john.rohrlich@colorado.edu>
2016-07-28 03:39:13 +0000

    Remove ifdef _WINDOWS code for erf() and erfc() - not needed with VS2015 libraries

--- src/temt/ta_math/taMath_double.cpp
+++ src/temt/ta_math/taMath_double.cpp
@@ -66,31 +66,6 @@ bool taMath_double::isnan(double val) {
   return false;
 }
 
-#ifdef _WINDOWS
-//# include <stdlib.h>
-//# include "../../../3rdparty/misc/s_erf.c"
- // extern double erf(double x); // in s_erf.c
- // extern double erfc(double x); // in s_erf.c
-
-double erfc(double x) {
-  double t,z,ans;
-
-  z=fabs(x);
-  t=1.0/(1.0+0.5*z);
-  ans=t*exp(-z*z-1.26551223+t*(1.00002368+t*(0.37409196+t*(0.09678418+
-        t*(-0.18628806+t*(0.27886807+t*(-1.13520398+t*(1.48851587+
-        t*(-0.82215223+t*0.17087277)))))))));
-  return  x >= 0.0 ? ans : 2.0-ans;
-}
-
-double erf(double x) {
-  return 1.0 - erfc(x);
-}
-
-
-#endif
-
-
 /////////////////////////////////////////////////////////////////////////////////
 // ExpLog: exponential and logarithmic functions
 
