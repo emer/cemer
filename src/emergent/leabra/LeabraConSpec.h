228e09a889602d0633c5b8a6613f7e76be74bf8e
Randall O'Reilly <oreilly@ucdavis.edu>
2016-08-10 04:22:37 +0000

    it is either the bias weight or the inhib reset .. this fixes the bias weight dwt init

--- src/emergent/leabra/LeabraConSpec.h
+++ src/emergent/leabra/LeabraConSpec.h
@@ -592,8 +592,8 @@ public:
 
   // same as original:
   inline void B_Init_dWt(UnitVars* uv, Network* net, int thr_no) override {
-    //    C_Init_dWt(uv->bias_dwt);  // should be:
-    C_Init_dWt(uv->bias_wt);
+    C_Init_dWt(uv->bias_dwt);  // should be!
+    // C_Init_dWt(uv->bias_wt);
   }
 
   inline void B_Init_Weights_post(UnitVars* u, Network* net, int thr_no) override {
