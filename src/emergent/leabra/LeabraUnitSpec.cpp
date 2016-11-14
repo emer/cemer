657110f6391192bf6f716a90b88800be6381cfeb
Randall O'Reilly <oreilly@ucdavis.edu>
2016-11-14 23:53:10 +0000

    * m_in_plus option for integrating minus phase data in plus phase (which is normally what is done -- but looking at the avg_m vs avg_s states it is striking how little minus phase activation remains in avg_m). * also change act_diff to reflect avg_s_eff - avg_m -- the actual learning signal!

--- src/emergent/leabra/LeabraUnitSpec.cpp
+++ src/emergent/leabra/LeabraUnitSpec.cpp
@@ -194,6 +194,7 @@ void LeabraActAvgSpec::Initialize() {
 }
 
 void LeabraActAvgSpec::Defaults_init() {
+  m_in_plus = true;
   ss_tau = 2.0f;
   s_tau = 2.0f;
   m_tau = 10.0f;
@@ -1873,7 +1874,14 @@ void LeabraUnitSpec::Compute_SRAvg(LeabraUnitVars* u, LeabraNetwork* net, int th
 
   u->avg_ss += dt.integ * act_avg.ss_dt * (ru_act - u->avg_ss);
   u->avg_s += dt.integ * act_avg.s_dt * (u->avg_ss - u->avg_s);
-  u->avg_m += dt.integ * act_avg.m_dt * (u->avg_s - u->avg_m);
+  if(act_avg.m_in_plus) {
+    u->avg_m += dt.integ * act_avg.m_dt * (u->avg_s - u->avg_m);
+  }
+  else {
+    if(net->quarter < 3) {
+      u->avg_m += dt.integ * act_avg.m_dt * (u->avg_s - u->avg_m);
+    }
+  }
 
   u->avg_s_eff = act_avg.s_in_s * u->avg_s + act_avg.m_in_s * u->avg_m;
 }
@@ -2007,7 +2015,8 @@ void LeabraUnitSpec::Quarter_Final_RecVals(LeabraUnitVars* u, LeabraNetwork* net
   case 3:
     u->act_q4 = use_act;
     u->act_p = use_act;
-    u->act_dif = u->act_p - u->act_m;
+    //    u->act_dif = u->act_p - u->act_m;
+    u->act_dif = u->avg_s_eff - u->avg_m;
     Compute_ActTimeAvg(u, net, thr_no);
     break;
   }
