d929c4b3fbf88b6ea2d12e56baa389e8ac29d57d
Randall O'Reilly <oreilly@ucdavis.edu>
2016-11-16 04:58:56 +0000

    testing out new simpler adaptleak algo -- again just another running average instead of thresholds..

--- src/emergent/leabra/LeabraUnitSpec.h
+++ src/emergent/leabra/LeabraUnitSpec.h
@@ -331,24 +331,20 @@ class E_API AdaptLeakSpec : public SpecMemberBase {
 INHERITED(SpecMemberBase)
 public:
   bool          on;             // perform leak adaptation?  if so, adapts bias weight as an increment to default g_bar.l leak value as function of computed avg_l values which reflect longer-term average activation of unit -- be sure to turn learning off when using this!
-  float         tau;           // #CONDSHOW_ON_on #DEF_100 time constant for adapting the leak as a function of avg_l value
-  float         hi_thr;        // #CONDSHOW_ON_on #DEF_1:1.2 high-side threshold on avg_l value -- avg_l must exceed this value before increasing bias weight offset to leak value
-  float         lo_thr;        // #CONDSHOW_ON_on #DEF_0.4 low-side threshold on avg_l value -- avg_l must be below this value before decreasing bias weight offset to leak value
-  float         min_bwt;      // #CONDSHOW_ON_on #DEF_0;-0.05 minimum leak offset (i.e., bias weight or bwt) value -- don't want too decrease leak too much below default parameter in general
-  float         max_bwt;      // #CONDSHOW_ON_on #DEF_0.2 maximum leak offset (i.e., bias weight or bwt) value -- prevents excessive modification for situations where units must be active
+  float         tau;           // #CONDSHOW_ON_on #DEF_100 time constant for adapting the leak as a function of avg_l value -- just does a longer term running average of the avg_l value
+  float         gain;          // #CONDSHOW_ON_on multiplier on the avg_l value to turn it into the leak value -- roughly determines the maximum amount of additional leak that is applied
+  
+  // float         hi_thr;        // #CONDSHOW_ON_on #DEF_1:1.2 high-side threshold on avg_l value -- avg_l must exceed this value before increasing bias weight offset to leak value
+  // float         lo_thr;        // #CONDSHOW_ON_on #DEF_0.4 low-side threshold on avg_l value -- avg_l must be below this value before decreasing bias weight offset to leak value
+  // float         min_bwt;      // #CONDSHOW_ON_on #DEF_0;-0.05 minimum leak offset (i.e., bias weight or bwt) value -- don't want too decrease leak too much below default parameter in general
+  // float         max_bwt;      // #CONDSHOW_ON_on #DEF_0.2 maximum leak offset (i.e., bias weight or bwt) value -- prevents excessive modification for situations where units must be active
 
   float         dt;            // #CONDSHOW_ON_on #READ_ONLY #EXPERT rate =  1 / tau
-  float         mid_thr;       // #CONDSHOW_ON_on #READ_ONLY #EXPERT middle point between hi and low threshold
+  // float         mid_thr;       // #CONDSHOW_ON_on #READ_ONLY #EXPERT middle point between hi and low threshold
 
-  inline bool   AdaptLeak(float& bwt, float& dwt, const float avg_l) {
-    if(avg_l >= hi_thr || avg_l <= lo_thr) {
-      dwt = dt * (avg_l - mid_thr);
-      bwt += dwt;
-      if(bwt < min_bwt)        bwt = min_bwt;
-      else if(bwt > max_bwt)   bwt = max_bwt;
-      return true;
-    }
-    return false;
+  inline void   AdaptLeak(float& bwt, float& dwt, const float avg_l) {
+    dwt = dt * (gain * avg_l - bwt);
+    bwt += dwt;
   }
   // adadpt leak offset as bias weight values, given current avg_l value
   
