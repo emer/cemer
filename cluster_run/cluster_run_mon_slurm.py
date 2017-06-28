142015e14944bb03498058a3a1809d54682b2c7d
Kai Krueger <kakrueger@gmail.com>
2017-06-28 04:32:29 +0000

    Undo a previous wrong commit to cluster_run_mon_slurm.py that changed the local settings

--- cluster_run/cluster_run_mon_slurm.py
+++ cluster_run/cluster_run_mon_slurm.py
@@ -9,7 +9,7 @@ import cluster_run_mon_lib as runmon
 # STANDARD USER CONFIGURABLE PARAMETERS
 
 # name of queue -- used for a few things -- replace with actual!
-runmon.clust_queue = "local"
+runmon.clust_queue = "blanca-ccn"
 
 # specifies if the cluster uses QoS or partitions to specify the queue. This is only relevant for SLURM
 runmon.use_qos = True
@@ -35,9 +35,13 @@ runmon.use_cuda = False
 # specify the job launcher command with all of its parameters. The parameters might be
 # specific to the cluster environment, such as the network interface the MPI libraries should use
 #job_launcher = "mpirun"
+runmon.job_launcher = 'mpirun --bind-to none'
 #runmon.job_launcher = 'mpirun --bind-to none --mca btl_tcp_if_include bond0'
-runmon.job_launcher = 'mpirun -v --debug-daemons --bind-to none --mca btl_base_verbose 1 --mca btl_base_debug 2 --mca btl_openib_verbose 1 --mca btl_tcp_if_include bond0'
 
+# specify a setup script that is run at the beginning of a job on the node the job is executed,
+# this allows to ensure that all of the enivronment variables, paths and other resources necessary
+# on the compute node is available and correctly setup.
+runmon.path_setup = "/work/ccnlab/bin/emergent_setup.sh"
 
 # qstat-like command -- for quering a specific job_no 
 # sge = qstat -j <job_no>
@@ -78,14 +82,8 @@ runmon.mail_user = "$USER"
 runmon.mail_type = "FAIL"
 
 # set to true for more debugging info
-runmon.debug = True
+runmon.debug = False
 # runmon.debug = True
-
-# specify the email address of the user to notify of job events
-runmon.mail_user = '$USER'
-# specify the type of job events the user should be notified about via email
-runmon.mail_type = "FAIL"
-
 
 # END OF STANDARD USER CONFIGURABLE PARAMETERS
 #############################################################################
