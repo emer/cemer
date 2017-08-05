2a1bfbf483593dd7e2a200aeb6fa2c6b928967c8
John Rohrlich <john.rohrlich@colorado.edu>
2017-08-05 03:04:10 +0000

    Some smarts for the ColCalcExpr completer * lists column names * column_name. gets you a list of string matching routines if column type is String (contains, startsWith, endsWith, matches, matches_regexp, matches_wildcard) and each is followed by (" so users don't type (' which won't work * column names will not appear if the prior portion of the expression was another column name but will appear if you have column name and then operator * So a bit of help for the user but by no means a full parser/completer

--- src/temt/ta_data/ColCalcExpr.cpp
+++ src/temt/ta_data/ColCalcExpr.cpp
@@ -76,25 +76,86 @@ Completions* ColCalcExpr::StringFieldLookupForCompleter(const String& cur_txt, i
                                                         const String& mbr_name, int& new_pos) {
   completions.Reset();
   
-  if (data_cols) {
-    for (int i=0; i<data_cols->size; i++) {
-      DataCol* col = data_cols->SafeEl(i);
-      completions.object_completions.Link(col);
+  bool space = false;  // true if there is a space before the cursor and after any '.'
+  bool dot = false;    // true if there is a '.' before the cursor and after any spas
+  
+  int space_pos;  // -1 if not found
+  int dot_pos;    // -1 if not found
+  
+  String pre_cursor_text;
+  pre_cursor_text = cur_txt.through(cur_pos);
+  space_pos = pre_cursor_text.index(' ', -1);
+  dot_pos = pre_cursor_text.index('.', -1);
+  
+  String expression = pre_cursor_text;  // assume until we check if space
+  if (space_pos > dot_pos) {
+    space = true;
+    expression = pre_cursor_text.after(space_pos);
+  }
+  else if (dot_pos > space_pos) {
+    dot = true;
+    if (space_pos != -1) { // if there is a space before the dot
+      expression = pre_cursor_text.after(space_pos);
     }
   }
-  completions.pre_cursor_text = cur_txt.through(cur_pos);
-  int pos = completions.pre_cursor_text.index(' ', -1);
-  if (pos == -1) {
-    completions.seed = cur_txt.through(cur_pos);
-    completions.pre_text = "";
-}
   else {
-    completions.seed = cur_txt.after(' ', -1);
-    completions.pre_text = cur_txt.before(' ', -1);
+    ; // nothing to do - neither space nor dot
   }
   
+  if (dot) {
+    String column_name = expression.before(dot_pos);
+    DataCol* col = data_cols->FindName(column_name);
+    if (col && col->isString()) {
+      completions.string_completions.Add("contains(\"");
+      completions.string_completions.Add("startsWith(\"");
+      completions.string_completions.Add("endsWith(\"");
+      completions.string_completions.Add("matches(\"");
+      completions.string_completions.Add("matches_regexp(\"");
+      completions.string_completions.Add("matches_wildcard(\"");
+      
+      completions.seed = expression.after(dot_pos);
+      completions.pre_text = expression;
+    }
+  }
+  else {
+    String prior_expression = cur_txt.before(space_pos);
+    if (prior_expression.empty()) {
+      if (data_cols) {
+        for (int i=0; i<data_cols->size; i++) {
+          DataCol* col = data_cols->SafeEl(i);
+          completions.object_completions.Link(col);
+        }
+      }
+      completions.seed = expression;
+      completions.pre_text = expression;
+    }
+    else {  // was the prior expression a column? if so don't list the column
+      int prior_space_pos = prior_expression.index(' ', -1);
+      if (prior_space_pos != -1) {
+        prior_expression = prior_expression.after(prior_space_pos);
+      }
+      if (prior_expression.contains('.')) {  // the column name will be before first '.' in the expression
+        prior_expression = prior_expression.before(prior_expression.index('.'));
+      }
+      // if not a column must be operator or such - ok to add columns to completion list
+      DataCol* col = data_cols->FindName(prior_expression);
+      if (!col) {
+        if (data_cols) {
+          for (int i=0; i<data_cols->size; i++) {
+          col = data_cols->SafeEl(i);
+            completions.object_completions.Link(col);
+          }
+        }
+        completions.seed = expression;
+        completions.pre_text = expression;
+      }
+    }
+  }
+  
+  completions.pre_cursor_text = cur_txt.through(cur_pos);
   return &completions;
 }
+
 
 
 
