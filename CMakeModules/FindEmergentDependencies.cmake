d360e80d7a683921d6cf02b7e345a493e7a3b425
Randall O'Reilly <oreilly@ucdavis.edu>
2017-08-13 18:03:45 +0000

    remove quarter comments to see if that fixes john's build issue

--- CMakeModules/FindEmergentDependencies.cmake
+++ CMakeModules/FindEmergentDependencies.cmake
@@ -223,7 +223,7 @@ ENDIF(CUDA_BUILD)
 # NOTE: could also do BISON but it is not really required so not worth the hassle
 #find_package(BISON)
 
-include_directories(${COIN_INCLUDE_DIR} # ${QUARTER_INCLUDE_DIR}
+include_directories(${COIN_INCLUDE_DIR} 
   ${ODE_INCLUDE_DIR}
   ${GSL_INCLUDE_DIR}
   ${SUBVERSION_INCLUDE_DIRS}
@@ -240,12 +240,12 @@ endif (WIN32)
 
 # Windows dll macros
 if (WIN32)
-  add_definitions(-DCOIN_DLL) #-DQUARTER_DLL)
+  add_definitions(-DCOIN_DLL)
 endif (WIN32)
 
 # all dependency libraries to link to -- used automatically in EMERGENT_LINK_LIBRARIES
 # specify in executables
-set(EMERGENT_DEP_LIBRARIES ${COIN_LIBRARY} ${QT_LIBRARIES} # ${QUARTER_LIBRARY} 
+set(EMERGENT_DEP_LIBRARIES ${COIN_LIBRARY} ${QT_LIBRARIES}
     ${ODE_LIBRARY} ${GSL_LIBRARIES}
     ${OPENGL_LIBRARIES} ${ZLIB_LIBRARIES}
     ${SUBVERSION_LIBRARIES} ${EMERGENT_OPT_LIBRARIES}
