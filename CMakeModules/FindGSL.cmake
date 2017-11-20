3bfd9adcce46f1195da83c890b2cec769a6f0878
Randall O'Reilly <oreilly@ucdavis.edu>
2017-11-20 06:41:36 +0000

    findgsl don't load std args

--- CMakeModules/FindGSL.cmake
+++ CMakeModules/FindGSL.cmake
@@ -56,7 +56,8 @@
 #  GSL_LIBRARY_DEBUG       - Location of the debug GSL library (if any).
 #
 
-include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+# seems like we can skip this
+#include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
 
 #=============================================================================
 # If the user has provided ``GSL_ROOT_DIR``, use it!  Choose items found
